b46584eb3db415a715f39612765651049a95c8fc
ctotone <ctotone@gmail.com>
2022-11-30 00:15:24 +0100

    Push avec maccro

---
+++ maccro/test compétence GM.java
@@ -0,0 +1,108 @@
+?{Choix de la compétence|
+,|
+--------- STATISTIQUES ----,
+|
+Force ,/w gm &{template:stats&#125; {{header=Force&#125;&#125; {{subheader=[[@{selected|modFORPJ}+@{selected|strength}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125; {{stat=[[@{selected|modFORPJ}/5+@{selected|strength_score}]]&#125;&#125; {{score=[[@{selected|modFORPJ}+@{selected|strength}]]&#125;&#125;
+|
+Constitution,/w gm &{template:stats&#125; {{header=Constitution&#125;&#125; {{subheader=[[@{selected|modCONPJ}+@{selected|constitution}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125; {{stat=[[@{selected|modCONPJ}/5+@{selected|constitution_score}]]&#125;&#125; {{score=[[@{selected|modCONPJ}+@{selected|constitution}]]&#125;&#125;,
+|
+Dextérité,/w gm &{template:stats&#125; {{header=Dextérité&#125;&#125; {{subheader=[[@{selected|modDEXPJ}+@{selected|dexterity}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125; {{stat=[[@{selected|modDEXPJ}/5+@{selected|dexterity_score}]]&#125;&#125; {{score=[[@{selected|modDEXPJ}+@{selected|dexterity}]]&#125;&#125;,
+|
+Intelligence,/w gm &{template:stats&#125; {{header=Intelligence&#125;&#125; {{subheader=[[@{selected|modINTPJ}+@{selected|intelligence}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125; {{stat=[[@{selected|modINTPJ}/5+@{selected|intelligence_score}]]&#125;&#125; {{score=[[@{selected|modINTPJ}+@{selected|intelligence}]]&#125;&#125;,
+|
+Pouvoir,/w gm &{template:stats&#125; {{header=Pouvoir&#125;&#125; {{subheader=[[@{selected|modPOUPJ}+@{selected|power}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125; {{stat=[[@{selected|modPOUPJ}/5+@{selected|power_score}]]&#125;&#125; {{score=[[@{selected|modPOUPJ}+@{selected|power}]]&#125;&#125;,
+|
+Charisme,/w gm &{template:stats&#125; {{header=Charisme&#125;&#125; {{subheader=[[@{selected|modCARPJ}+@{selected|charisma}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125; {{stat=[[@{selected|modCARPJ}/5+@{selected|charisma_score}]]&#125;&#125; {{score=[[@{selected|modCARPJ}+@{selected|charisma}]]&#125;&#125;,
+|
+Chance,/w gm &{template:rolls&#125; {{header=Chance&#125;&#125; {{subheader=@{selected|luck}&#125;&#125; {{rating=@{selected|luck}&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Santé mentale,/w gm &{template:rolls&#125; {{header=Jet de Santé mentale&#125;&#125;  {{dice=[[1d100]]&#125;&#125;
+/em rappelle toi que Réussite et Échec critique existent sur les tests de Santé mentale ..., 
+|
+--------- COMPÉTENCES ----,
+|
+Administration,/w gm &{template:rolls&#125; {{header=Administration&#125;&#125; {{subheader=[[@{selected|bureaucracyPJ}+@{selected|bureaucracy}]]&#125;&#125; {{rating=[[@{selected|bureaucracyPJ}+@{selected|bureaucracy}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Anthropologie,/w gm &{template:rolls&#125; {{header=Anthropologie&#125;&#125; {{subheader=[[@{selected|anthropologyPJ}+@{selected|anthropology}]]&#125;&#125; {{rating=[[@{selected|anthropologyPJ}+@{selected|anthropology}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Archéologie,/w gm &{template:rolls&#125; {{header=Archéologie&#125;&#125; {{subheader=[[@{selected|archeologyPJ}+@{selected|archeology}]]&#125;&#125; {{rating=[[@{selected|archeologyPJ}+@{selected|archeology}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Armes à feu,/w gm &{template:rolls&#125; {{header=Armes à feu&#125;&#125; {{subheader=[[@{selected|firearmsPJ}+@{selected|firearms}]]&#125;&#125; {{rating=[[@{selected|firearmsPJ}+@{selected|firearms}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Armes de mêlée,/w gm &{template:rolls&#125; {{header=Armes de mêlée&#125;&#125; {{subheader=[[@{selected|melee_weaponsPJ}+@{selected|melee_weapons}]]&#125;&#125; {{rating=[[@{selected|melee_weaponsPJ}+@{selected|melee_weapons}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Armes lourdes,/w gm &{template:rolls&#125; {{header=Armes lourdes&#125;&#125; {{subheader=[[@{selected|heavy_weaponsPJ}+@{selected|heavy_weapons}]]&#125;&#125; {{rating=[[@{selected|heavy_weaponsPJ}+@{selected|heavy_weapons}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Art,/w gm &{template:rolls&#125; {{header=Art&#125;&#125; {{subheader=[[@{selected|artPJ}+@{selected|art}]]&#125;&#125; {{rating=[[@{selected|artPJ}+@{selected|art}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Artillerie,/w gm &{template:rolls&#125; {{header=Artillerie&#125;&#125; {{subheader=[[@{selected|artilleryPJ}+@{selected|artillery}]]&#125;&#125; {{rating=[[@{selected|artilleryPJ}+@{selected|artillery}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Artisanat,/w gm &{template:rolls&#125; {{header=Artisanat&#125;&#125; {{subheader=[[@{selected|craftPJ}+@{selected|craft}]]&#125;&#125; {{rating=[[@{selected|craftPJ}+@{selected|craft}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Athlétisme,/w gm &{template:rolls&#125; {{header=Athlétisme&#125;&#125; {{subheader=[[@{selected|athleticsPJ}+@{selected|athletics}]]&#125;&#125; {{rating=[[@{selected|athleticsPJ}+@{selected|athletics}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Chirurgie,/w gm &{template:rolls&#125; {{header=Chirurgie&#125;&#125; {{subheader=[[@{selected|surgeryPJ}+@{selected|surgery}]]&#125;&#125; {{rating=[[@{selected|surgeryPJ}+@{selected|surgery}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Comptabilité,/w gm &{template:rolls&#125; {{header=Comptabilité&#125;&#125; {{subheader=[[@{selected|accountingPJ}+@{selected|accounting}]]&#125;&#125; {{rating=[[@{selected|accountingPJ}+@{selected|accounting}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Conduite,/w gm &{template:rolls&#125; {{header=Conduite&#125;&#125; {{subheader=[[@{selected|drivePJ}+@{selected|drive}]]&#125;&#125; {{rating=[[@{selected|drivePJ}+@{selected|drive}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Corps à corps,/w gm &{template:rolls&#125; {{header=Coprs à corps&#125;&#125; {{subheader=[[@{selected|unarmed_combatPJ}+@{selected|unarmed_combat}]]&#125;&#125; {{rating=[[@{selected|unarmed_combatPJ}+@{selected|unarmed_combat}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Criminologie,/w gm &{template:rolls&#125; {{header=Criminologie&#125;&#125; {{subheader=[[@{selected|criminologyPJ}+@{selected|criminology}]]&#125;&#125; {{rating=[[@{selected|criminologyPJ}+@{selected|criminology}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Déguisement,/w gm &{template:rolls&#125; {{header=Déguisement&#125;&#125; {{subheader=[[@{selected|disguisePJ}+@{selected|disguise}]]&#125;&#125; {{rating=[[@{selected|disguisePJ}+@{selected|disguise}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Démolition,/w gm &{template:rolls&#125; {{header=Démolition&#125;&#125; {{subheader=[[@{selected|demolitionsPJ}+@{selected|demolitions}]]&#125;&#125; {{rating=[[@{selected|demolitionsPJ}+@{selected|demolitions}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Discrétion,/w gm &{template:rolls&#125; {{header=Discrétion&#125;&#125; {{subheader=[[@{selected|stealthPJ}+@{selected|stealth}]]&#125;&#125; {{rating=[[@{selected|stealthPJ}+@{selected|stealth}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Équitation,/w gm &{template:rolls&#125; {{header=Équitation&#125;&#125; {{subheader=[[@{selected|ridePJ}+@{selected|ride}]]&#125;&#125; {{rating=[[@{selected|ridePJ}+@{selected|ride}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Esquive,/w gm &{template:rolls&#125; {{header=Esquive&#125;&#125; {{subheader=[[@{selected|dodgePJ}+@{selected|dodge}]]&#125;&#125; {{rating=[[@{selected|dodgePJ}+@{selected|dodge}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Expertise militaire,/w gm &{template:rolls&#125; {{header=Expertise militaire&#125;&#125; {{subheader=[[@{selected|military_sciencePJ}+@{selected|military_science}]]&#125;&#125; {{rating=[[@{selected|military_sciencePJ}+@{selected|military_science}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Histoire,/w gm &{template:rolls&#125; {{header=Histoire&#125;&#125; {{subheader=[[@{selected|historyPJ}+@{selected|history}]]&#125;&#125; {{rating=[[@{selected|historyPJ}+@{selected|history}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+HUMINT,/w gm &{template:rolls&#125; {{header=HUMINT&#125;&#125; {{subheader=[[@{selected|HUMINTPJ}+@{selected|HUMINT}]]&#125;&#125; {{rating=[[@{selected|HUMINTPJ}+@{selected|HUMINT}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Informatique,/w gm &{template:rolls&#125; {{header=Informatique&#125;&#125; {{subheader=[[@{selected|computer_sciencePJ}+@{selected|computer_science}]]&#125;&#125; {{rating=[[@{selected|computer_sciencePJ}+@{selected|computer_science}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Loi et droit,/w gm &{template:rolls&#125; {{header=Loi et droit&#125;&#125; {{subheader=[[@{selected|lawPJ}+@{selected|law}]]&#125;&#125; {{rating=[[@{selected|lawPJ}+@{selected|law}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Machinerie lourde,/w gm &{template:rolls&#125; {{header=Machinerie lourde&#125;&#125; {{subheader=[[@{selected|heavy_machineryPJ}+@{selected|heavy_machinery}]]&#125;&#125; {{rating=[[@{selected|heavy_machineryPJ}+@{selected|heavy_machinery}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Médecine,/w gm &{template:rolls&#125; {{header=Médecine&#125;&#125; {{subheader=[[@{selected|medicinePJ}+@{selected|medicine}]]&#125;&#125; {{rating=[[@{selected|medicinePJ}+@{selected|medicine}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Médecine légale,/w gm &{template:rolls&#125; {{header=Médecine légale&#125;&#125; {{subheader=[[@{selected|forensicsPJ}+@{selected|forensics}]]&#125;&#125; {{rating=[[@{selected|forensicsPJ}+@{selected|forensics}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Natation,/w gm &{template:rolls&#125; {{header=Natation&#125;&#125; {{subheader=[[@{selected|swimPJ}+@{selected|swim}]]&#125;&#125; {{rating=[[@{selected|swimPJ}+@{selected|swim}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Navigation,/w gm &{template:rolls&#125; {{header=Navigation&#125;&#125; {{subheader=[[@{selected|navigatePJ}+@{selected|navigate}]]&#125;&#125; {{rating=[[@{selected|navigatePJ}+@{selected|navigate}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Occultisme,/w gm &{template:rolls&#125; {{header=Occultisme&#125;&#125; {{subheader=[[@{selected|occultPJ}+@{selected|occult}]]&#125;&#125; {{rating=[[@{selected|occultPJ}+@{selected|occult}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Paranormal,/w gm &{template:rolls&#125; {{header=Paranormal&#125;&#125; {{subheader=[[@{selected|unnaturalPJ}+@{selected|unnatural}]]&#125;&#125; {{rating=[[@{selected|unnaturalPJ}+@{selected|unnatural}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Persuasion,/w gm &{template:rolls&#125; {{header=Persuasion&#125;&#125; {{subheader=[[@{selected|persuadePJ}+@{selected|persuade}]]&#125;&#125; {{rating=[[@{selected|persuadePJ}+@{selected|persuade}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Pharmacie,/w gm &{template:rolls&#125; {{header=Pharmacie&#125;&#125; {{subheader=[[@{selected|pharmacyPJ}+@{selected|pharmacy}]]&#125;&#125; {{rating=[[@{selected|pharmacyPJ}+@{selected|pharmacy}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Pilotage,/w gm &{template:rolls&#125; {{header=Pilotage&#125;&#125; {{subheader=[[@{selected|pilotPJ}+@{selected|pilot}]]&#125;&#125; {{rating=[[@{selected|pilotPJ}+@{selected|pilot}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Premiers secours,/w gm &{template:rolls&#125; {{header=Premiers soins&#125;&#125; {{subheader=[[@{selected|first_aidPJ}+@{selected|first_aid}]]&#125;&#125; {{rating=[[@{selected|first_aidPJ}+@{selected|first_aid}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Psychotérapie,/w gm &{template:rolls&#125; {{header=Psychotérapie&#125;&#125; {{subheader=[[@{selected|psychotherapyPJ}+@{selected|psychotherapy}]]&#125;&#125; {{rating=[[@{selected|psychotherapyPJ}+@{selected|psychotherapy}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Recherche,/w gm &{template:rolls&#125; {{header=Recherche&#125;&#125; {{subheader=[[@{selected|searchPJ}+@{selected|search}]]&#125;&#125; {{rating=[[@{selected|searchPJ}+@{selected|search}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Science,/w gm &{template:rolls&#125; {{header=Science&#125;&#125; {{subheader=[[@{selected|pilotPJ}+@{selected|pilot}]]&#125;&#125; {{rating=[[@{selected|pilotPJ}+@{selected|pilot}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+SIGINT,/w gm &{template:rolls&#125; {{header=SIGINT&#125;&#125; {{subheader=[[@{selected|sigintPJ}+@{selected|sigint}]]&#125;&#125; {{rating=[[@{selected|sigintPJ}+@{selected|sigint}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Survie,/w gm &{template:rolls&#125; {{header=Survie&#125;&#125; {{subheader=[[@{selected|survivalPJ}+@{selected|survival}]]&#125;&#125; {{rating=[[@{selected|survivalPJ}+@{selected|survival}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+|
+Vigilance,/w gm &{template:rolls&#125; {{header=Vigilance&#125;&#125; {{subheader=[[@{selected|alertnessPJ}+@{selected|alertness}]]&#125;&#125; {{rating=[[@{selected|alertnessPJ}+@{selected|alertness}]]&#125;&#125; {{dice=[[1d100]]&#125;&#125;,
+
+}
---
+++ maccro/test compétence caché.java
@@ -0,0 +1,55 @@
+?{Choix de la compétence|
+,|
+--------- STATISTIQUES ----,
+|
+Pouvoir,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Pouvoir=[[@{selected|modPOUPJ}+@{selected|power}]]&#125;&#125;
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+!blindroll 1d100
+|
+Charisme,&{template:default&#125; {{name=@{selected|name}&#125;&#125;{{Score de Charisme=[[@{selected|modCARPJ}+@{selected|charisma}]]&#125;&#125;
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+!blindroll 1d100
+|
+Chance,&{template:default&#125; {{name=@{selected|name}&#125;&#125;{{Score de Chance=[[@{selected|luck}]]&#125;&#125;
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+!blindroll 1d100
+|
+--------- COMPÉTENCES ----,
+|
+Déguisement,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Déguisement=[[@{selected|disguisePJ}+@{selected|disguise}]]&#125;&#125;
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+!blindroll 1d100
+|
+Discrétion,&{template:default&#125; {{name=@{selected|name}n&#125;&#125; {{Score de Discrétion=[[@{selected|stealthPJ}+@{selected|stealth}]]&#125;&#125;
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+!blindroll 1d100
+|
+HUMINT,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de HUMINT=[[@{selected|HUMINTPJ}+@{selected|HUMINT}]]&#125;&#125;
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+!blindroll 1d100
+|
+Occultisme,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score d'Occultisme=[[@{selected|occultPJ}+@{selected|occult}]]&#125;&#125;
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+!blindroll 1d100
+|
+Paranormal,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Paranormal=[[@{selected|unnaturalPJ}+@{selected|unnatural}]]&#125;&#125;
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+!blindroll 1d100
+|
+Persuasion,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Persuasion=[[@{selected|persuadePJ}+@{selected|persuade}]]&#125;&#125;
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+!blindroll 1d100
+|
+Psychotérapie,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Psychotérapie=[[@{selected|psychotherapyPJ}+@{selected|psychotherapy}]]&#125;&#125;
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+!blindroll 1d100
+|
+Recherche,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Recherche=[[@{selected|searchPJ}+@{selected|search}]]&#125;&#125;
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+!blindroll 1d100
+|
+Vigilance,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Vigilance=[[@{selected|alertnessPJ}+@{selected|alertness}]]&#125;&#125;
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+!blindroll 1d100
+
+}
