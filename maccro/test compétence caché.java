b707e07046976ec6c86d2fd120a6c002913c5f4a
ctotone <ctotone@gmail.com>
2022-12-16 18:10:45 +0100

    MAccro avec template custom

--- maccro/test compétence caché.java
+++ maccro/test compétence caché.java
@@ -2,53 +2,53 @@
 ,|
 --------- STATISTIQUES ----,
 |
-Pouvoir,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Pouvoir=[[@{selected|modPOUPJ}+@{selected|power}]]&#125;&#125;
+Pouvoir,&{template:custom&#125; {{color=black&#125;&#125;{{title=Pouvoir&#125;&#125;{{subtitle=@{selected|name}&#125;&#125; {{Score de Pouvoir=[[@{selected|modPOUPJ}+@{selected|power}]]&#125;&#125; {{Résultat transmis au MJ...&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
 !blindroll 1d100
 |
-Charisme,&{template:default&#125; {{name=@{selected|name}&#125;&#125;{{Score de Charisme=[[@{selected|modCARPJ}+@{selected|charisma}]]&#125;&#125;
+Charisme,&{template:custom&#125; {{color=black&#125;&#125;{{title=Charisme&#125;&#125;{{subtitle=@{selected|name}&#125;&#125;{{Score de Charisme=[[@{selected|modCARPJ}+@{selected|charisma}]]&#125;&#125; {{Résultat transmis au MJ...&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
 !blindroll 1d100
 |
-Chance,&{template:default&#125; {{name=@{selected|name}&#125;&#125;{{Score de Chance=[[@{selected|luck}]]&#125;&#125;
+Chance,&{template:custom&#125; {{color=black&#125;&#125;{{title=Chance&#125;&#125;{{subtitle=@{selected|name}&#125;&#125;{{Score de Chance=[[@{selected|luck}]]&#125;&#125; {{Résultat transmis au MJ...&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
 !blindroll 1d100
 |
 --------- COMPÉTENCES ----,
 |
-Déguisement,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Déguisement=[[@{selected|disguisePJ}+@{selected|disguise}]]&#125;&#125;
+Déguisement,&{template:custom&#125; {{color=black&#125;&#125;{{title=Déguisement&#125;&#125;{{subtitle=@{selected|name}&#125;&#125; {{Score de Déguisement=[[@{selected|disguisePJ}+@{selected|disguise}]]&#125;&#125; {{Résultat transmis au MJ...&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
 !blindroll 1d100
 |
-Discrétion,&{template:default&#125; {{name=@{selected|name}n&#125;&#125; {{Score de Discrétion=[[@{selected|stealthPJ}+@{selected|stealth}]]&#125;&#125;
+Discrétion,&{template:custom&#125; {{color=black&#125;&#125;{{title=Discrétion&#125;&#125;{{subtitle=@{selected|name}&#125;&#125; {{Score de Discrétion=[[@{selected|stealthPJ}+@{selected|stealth}]]&#125;&#125; {{Résultat transmis au MJ...&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
 !blindroll 1d100
 |
-HUMINT,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de HUMINT=[[@{selected|HUMINTPJ}+@{selected|HUMINT}]]&#125;&#125;
+HUMINT,&{template:custom&#125; {{color=black&#125;&#125;{{title=HUMINT&#125;&#125;{{subtitle=@{selected|name}&#125;&#125; {{Score de HUMINT=[[@{selected|HUMINTPJ}+@{selected|HUMINT}]]&#125;&#125; {{Résultat transmis au MJ...&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
 !blindroll 1d100
 |
-Occultisme,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score d'Occultisme=[[@{selected|occultPJ}+@{selected|occult}]]&#125;&#125;
+Occultisme,&{template:custom&#125; {{color=black&#125;&#125;{{title=Occultisme&#125;&#125;{{subtitle=@{selected|name}&#125;&#125; {{Score d'Occultisme=[[@{selected|occultPJ}+@{selected|occult}]]&#125;&#125; {{Résultat transmis au MJ...&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
 !blindroll 1d100
 |
-Paranormal,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Paranormal=[[@{selected|unnaturalPJ}+@{selected|unnatural}]]&#125;&#125;
+Paranormal,&{template:custom&#125; {{color=black&#125;&#125;{{title=Paranormal&#125;&#125;{{subtitle=@{selected|name}&#125;&#125; {{Score de Paranormal=[[@{selected|unnaturalPJ}+@{selected|unnatural}]]&#125;&#125; {{Résultat transmis au MJ...&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
 !blindroll 1d100
 |
-Persuasion,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Persuasion=[[@{selected|persuadePJ}+@{selected|persuade}]]&#125;&#125;
+Persuasion,&{template:custom&#125; {{color=black&#125;&#125;{{title=Persuasion&#125;&#125;{{subtitle=@{selected|name}&#125;&#125; {{Score de Persuasion=[[@{selected|persuadePJ}+@{selected|persuade}]]&#125;&#125; {{Résultat transmis au MJ...&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
 !blindroll 1d100
 |
-Psychotérapie,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Psychotérapie=[[@{selected|psychotherapyPJ}+@{selected|psychotherapy}]]&#125;&#125;
+Psychotérapie,&{template:custom&#125; {{color=black&#125;&#125;{{title=Psychotérapie&#125;&#125;{{subtitle=@{selected|name}&#125;&#125; {{Score de Psychotérapie=[[@{selected|psychotherapyPJ}+@{selected|psychotherapy}]]&#125;&#125; {{Résultat transmis au MJ...&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
 !blindroll 1d100
 |
-Recherche,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Recherche=[[@{selected|searchPJ}+@{selected|search}]]&#125;&#125;
+Recherche,&{template:custom&#125; {{color=black&#125;&#125;{{title=Recherche&#125;&#125;{{subtitle=@{selected|name}&#125;&#125; {{Score de Recherche=[[@{selected|searchPJ}+@{selected|search}]]&#125;&#125; {{Résultat transmis au MJ...&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
 !blindroll 1d100
 |
-Vigilance,&{template:default&#125; {{name=@{selected|name}&#125;&#125; {{Score de Vigilance=[[@{selected|alertnessPJ}+@{selected|alertness}]]&#125;&#125;
+Vigilance,&{template:custom&#125; {{color=black&#125;&#125;{{title=Vigilance&#125;&#125;{{subtitle=@{selected|name}&#125;&#125; {{Score de Vigilance=[[@{selected|alertnessPJ}+@{selected|alertness}]]&#125;&#125; {{Résultat transmis au MJ...&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
 !blindroll 1d100
 
