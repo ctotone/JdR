a0eaad60d55599248af767b4725e61ab06a8d1f7
ctotone <ctotone@gmail.com>
2023-02-26 14:07:50 +0100

    restructuration dossier

---
+++ Delta Green/maccro/reset MOD PJ.java
@@ -0,0 +1,9 @@
+!setattr --sel --silent --charid @{target|character_id}  --modFORPJ|0 --modCONPJ|0 --modCARPJ|0 --modPOUPJ|0 --modINTPJ|0 --modDEXPJ|0 --bureaucracyPJ|0  --anthropologyPJ|0 --archeologyPJ|0 --artPJ|0 --artilleryPJ|0 --craftPJ|0 --surgeryPJ|0 --accountingPJ|0 --drivePJ|0 --criminologyPJ|0 --disguisePJ|0 --demolitionsPJ|0 --ridePJ|0 --military_sciencePJ|0 --historyPJ|0 --HUMINTPJ|0 --computer_sciencePJ|0 --lawPJ|0 --heavy_machineryPJ|0 --medicinePJ|0 --forensicsPJ|0 --swimPJ|0 --navigatePJ|0 --occultPJ|0 --unnaturalPJ|0 --persuadePJ|0 --pharmacyPJ|0 --pilotPJ|0 --first_aidPJ|0 --psychotherapyPJ|0 --searchPJ|0 --sigintPJ|0 --survivalPJ|0 --firearmsPJ|0 --melee_weaponsPJ|0 --heavy_weaponsPJ|0 --athleticsPJ|0 --unarmed_combatPJ|0 --stealthPJ|0 --dodgePJ|0 --alertnessPJ|0 --sciencePJ|0
+
+
+
+
+
+
+
+
