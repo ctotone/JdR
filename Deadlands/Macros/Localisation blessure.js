1b572daaf0f9ddec5c4d1babc61623d2386ecb51
ctotone <ctotone@gmail.com>
2023-03-11 11:30:19 +0100

    Localisation joueur

---
+++ Deadlands/Macros/Localisation blessure.js
@@ -0,0 +1,5 @@
+// Version pour affichage GM
+/w gm &{template:custom}{{title=Localisation}}{{desc=[[1t[localisation]]]}}
+
+//version pour affichage en /em
+/em impacte sa cible dans [[1t[localisation]]].
