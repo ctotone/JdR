0449bcca88bede31f40bea0fc12ddee746f9d88a
ctotone <ctotone@gmail.com>
2023-03-11 11:10:35 +0100

    Maccro jet caché pour joueur avec !blindroll

---
+++ Deadlands/Macros/BlindRoll.js
@@ -0,0 +1,147 @@
+?{Choix de la compétence|
+,|
+--------- ATTRIBUTS ----,
+|
+Agilité (1D@{selected|agilite_de} + @{selected|agilite_bonus}),
+/em fait un jet caché d'Agilité
+!blindroll {1D@{selected|agilite_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|agilite_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}] - @{selected|encombrement}[@{selected|text_encombrement}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Âme (1D@{selected|ame_de} + @{selected|ame_bonus}),
+/em fait un jet caché d'Ame
+!blindroll {1D@{selected|ame_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|ame_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Force (1D@{selected|force_de} + @{selected|force_bonus}),
+/em fait un jet caché de Force
+!blindroll {1D@{selected|force_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|force_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}] - @{selected|encombrement}[@{selected|text_encombrement}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Intelligence (1D@{selected|intellect_de} + @{selected|ame_bonus}),
+/em fait un jet caché d'Intelligence
+!blindroll {1D@{selected|intellect_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|intellect_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Vigueur (1D@{selected|vigueur_de} + @{selected|ame_bonus}),
+/em fait un jet caché de Vigueur
+!blindroll {1D@{selected|vigueur_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|vigueur_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|,
+|
+--------- COMPÉTENCES ----,
+|
+Arcane (1D@{selected|arcane_de} + @{selected|arcane_bonus}),
+/em fait un jet caché d'Arcane
+!blindroll {1D@{selected|arcane_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|arcane_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Conduite (1D@{selected|conduite_de} + @{selected|conduite_bonus}),
+/em fait un jet caché de Conduite
+!blindroll {1D@{selected|conduite_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|conduite_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}] - @{selected|encombrement}[@{selected|text_encombrement}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Connaissance (1D@{selected|connaissance_de} + @{selected|connaissance_bonus}),
+/em fait un jet caché de Connaissance
+!blindroll {1D@{selected|connaissance_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|connaissance_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Crochetage (1D@{selected|crochetage_de} + @{selected|crochetage_bonus}),
+/em fait un jet caché de Crochetage
+!blindroll {1D@{selected|crochetage_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|crochetage_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}] - @{selected|encombrement}[@{selected|text_encombrement}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Discretion (1D@{selected|discretion_de} + @{selected|discretion_bonus}),
+/em fait un jet caché de Discrétion
+!blindroll {1D@{selected|discretion_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|discretion_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}] - @{selected|encombrement}[@{selected|text_encombrement}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Équitation (1D@{selected|equitation_de} + @{selected|equitation_bonus}),
+/em fait un jet caché d'Équitation
+!blindroll {1D@{selected|equitation_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|equitation_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}] - @{selected|encombrement}[@{selected|text_encombrement}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Escalade (1D@{selected|escalade_de} + @{selected|escalade_bonus}),
+/em fait un jet caché d'Escalade
+!blindroll {1D@{selected|escalade_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|escalade_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}] - @{selected|encombrement}[@{selected|text_encombrement}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Intimidation (1D@{selected|intimidation_de} + @{selected|intimidation_bonus}),
+/em fait un jet caché d'Intimidation
+!blindroll {1D@{selected|intimidation_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|intimidation_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Jeu (1D@{selected|jeu_de} + @{selected|jeu_bonus}),
+/em fait un jet caché de Jeu
+!blindroll {1D@{selected|jeu_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|jeu_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Lancer (1D@{selected|lancer_de} + @{selected|lancer_bonus}),
+/em fait un jet caché de Lancer
+!blindroll {1D@{selected|lancer_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|lancer_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}] - @{selected|encombrement}[@{selected|text_encombrement}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Natation (1D@{selected|natation_de} + @{selected|natation_bonus}),
+/em fait un jet caché de Natation
+!blindroll {1D@{selected|natation_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|natation_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}] - @{selected|encombrement}[@{selected|text_encombrement}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Navigation (1D@{selected|navigation_de} + @{selected|navigation_bonus}),
+/em fait un jet caché de Navigation
+!blindroll {1D@{selected|navigation_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|navigation_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}] - @{selected|encombrement}[@{selected|text_encombrement}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Perception (1D@{selected|perception_de} + @{selected|perception_bonus}),
+/em fait un jet caché de Perception
+!blindroll {1D@{selected|perception_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|perception_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Persuasion (1D@{selected|persuasion_de} + @{selected|persuasion_bonus}),
+/em fait un jet caché de Persuasion
+!blindroll {1D@{selected|persuasion_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|persuasion_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Pilotage (1D@{selected|pilotage_de} + @{selected|pilotage_bonus}),
+/em fait un jet caché de Pilotage
+!blindroll {1D@{selected|pilotage_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|pilotage_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}] - @{selected|encombrement}[@{selected|text_encombrement}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Pistage (1D@{selected|pistage_de} + @{selected|pistage_bonus}),
+/em fait un jet caché de Pistage
+!blindroll {1D@{selected|pistage_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|pistage_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Recherche (1D@{selected|recherche_de} + @{selected|recherche_bonus}),
+/em fait un jet caché de Recherche
+!blindroll {1D@{selected|recherche_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|recherche_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Réparation (1D@{selected|reparation_de} + @{selected|reparation_bonus}),
+/em fait un jet caché de Réparation
+!blindroll {1D@{selected|reparation_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|reparation_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Réseaux (1D@{selected|reseaux_de} + @{selected|reseaux_bonus}),
+/em fait un jet caché de Réseaux
+!blindroll {1D@{selected|reseaux_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|reseaux_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Sarcasme (1D@{selected|sarcasme_de} + @{selected|sarcasme_bonus}),
+/em fait un jet caché de Sarcasme
+!blindroll {1D@{selected|sarcasme_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|sarcasme_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Soins (1D@{selected|soins_de} + @{selected|soins_bonus}),
+/em fait un jet caché de Soins
+!blindroll {1D@{selected|soins_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|soins_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Survie (1D@{selected|survie_de} + @{selected|survie_bonus}),
+/em fait un jet caché de Survie
+!blindroll {1D@{selected|survie_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|survie_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+|
+Tripes (1D@{selected|tripes_de} + @{selected|tripes_bonus}),
+/em fait un jet caché de Tripes
+!blindroll {1D@{selected|tripes_de}!!&#44;@{selected|joker_de}!![@{selected|text_joker}]&#125;kh1 + @{selected|tripes_bonus}[@{selected|text_bonus}] - @{selected|malusblessures}[@{selected|text_blessures}] - @{selected|malusfatigue}[@{selected|text_fatigue}]
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés
+}
