61a00302b4bcfc7ff95c36954111e529cb03d750
ctotone <ctotone@gmail.com>
2023-03-13 22:48:54 +0100

    Create Son FX.js

---
+++ Deadlands/Macros/Son FX.js
@@ -0,0 +1,82 @@
+?{Choix de la compétence|
+,|
+---- Combat ----,
+|
+Coup de poing,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Coup de poing
+|
+Coup de poing1,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Coup de poing1
+|
+Explosion,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Explosion
+|
+Grosse explosion,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Grosse explosion
+|
+Pistolet,
+!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pistolet
+|
+Pistolet1,
+!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pistolet1 
+|
+Pistolet2,
+!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pistolet2
+|
+Pistolet3,
+!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pistolet3
+|
+Pompe,
+!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pompe
+|
+Sniper,
+!roll20AM --audio&#44;play&#44;nomenu&#124;GUN - Sniper
+|
+Grenade,
+!roll20AM --audio&#44;play&#44;nomenu&#124;Grenade
+|
+---- Transport ----,
+|
+Train western + klaxon,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Train western + klaxon 
+|
+Train western boucle,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Train western boucle 
+|
+Cheval au pas,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval au pas 
+|
+Cheval galop boucle,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval galop boucle
+|
+Cheval troupeau galop,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval troupeau galop
+|
+Chevaux - passage troupeau,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Chevaux - passage troupeau
+|
+Ane,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Ane
+|
+Cheval hennissement 1,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval hennissement 1
+|
+Cheval hennissement 2,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval hennissement 2
+|
+Cheval hennissement 3,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval hennissement 3
+|
+Cheval hénissement,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval hénissement 
+|
+Cheval galop (sur terre),
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval galop (sur terre) 
+|
+Calèche,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Calèche 
+|
+Jet de dés,
+!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés 
+
+}
