b41d55de54d77315cb1dc92c05c17fb590ce63b7
ctotone <ctotone@gmail.com>
2023-03-17 13:44:48 +0100

    Create FX-Ménagerie.js

---
+++ Deadlands/Macros/FX-Ménagerie.js
@@ -0,0 +1,20 @@
+?{Choix du son|
+,|
+--- Ménagerie ---,
+|
+Grognement léger,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Grogne léger
+|
+Grognement moyen,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Grogne moyen
+|
+Grognement aggressif,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Grogne aggressif
+|
+Chant du loup,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Chant loup
+|
+Croassement n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Corbeau 1
+|
+Croassement n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Corbeau 2
+|
+Vol Corbeau n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Envol corbeau 1
+|
+Vol Corbeau n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Envol corbeau 2
+}
