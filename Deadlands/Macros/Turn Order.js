241b675058a664afb1cdd06c245505291d8ec7a8
ctotone <ctotone@gmail.com>
2023-03-17 15:52:35 +0100

    Create Turn Order.js

---
+++ Deadlands/Macros/Turn Order.js
@@ -0,0 +1,13 @@
+// Macro pour regrouper toutes les commandes de DealInit
+
+
+?{Choix du son|
+,|
+Mélanger,!deal-init --reset --quiet
+|
+Distribuer,!deal-init
+|
+Tirer une carte,!deal-init --deal2chat
+|
+Fin Combat,!deal-init --clearTurnOrder
+}
