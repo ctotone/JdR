5da302074b9b65661635c299456e22bb057e1b44
ctotone <ctotone@gmail.com>
2023-03-17 13:43:04 +0100

    Create FX global.js

---
+++ Deadlands/Macros/FX global.js
@@ -0,0 +1,71 @@
+?{Choix du son|
+,|
+--- Ménagerie ---,
+Grognement léger,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Grogne léger
+|
+Grognement moyen,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Grogne moyen
+|
+Grognement aggressif,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Grogne aggressif
+|
+Chant du loup,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Chant loup
+|
+Croissement n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Corbeau 1
+|
+Croissement n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Corbeau 2
+|
+Vol Corbeau n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Envol corbeau 1
+|
+Vol Corbeau n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Envol corbeau 2
+|
+--- Animeaux ---,
+|
+Hénissement n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval hénnissement
+|
+Hénissement n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval hennissement 1
+|
+Hénissement n°3,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval hennissement 2
+|
+Hénissement n°4,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval hennissement 3
+|
+Cheval au pas,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval au pas
+|
+Cheval au galop n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval galop (sur terre)
+|
+Cheval au galop n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval galop boucle
+|
+--- Armes ---,
+|
+Pistolet n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pistolet
+|
+Pistolet n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pistolet1
+|
+Pistolet n°3,!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pistolet2
+|
+Pistolet n°4,!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pistolet3
+|
+Fusil à pompe,!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pompe
+|
+Arme de chasse,!roll20AM --audio&#44;play&#44;nomenu&#124;GUN - Sniper
+|
+Coup de poing n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Coup de poing
+|
+Coup de poing n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Coup de poing1
+|
+--- Hallelujah ---,
+|
+Cloche n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cloches 1
+|
+Cloche n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cloches 2
+|
+Magie n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Magie 1
+|
+Magie n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Magie 2
+|
+Magie n°3,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Magie 3
+|
+Magie n°4,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Magie 4
+|
+Magie n°5,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Magie 5
+
+
+}
