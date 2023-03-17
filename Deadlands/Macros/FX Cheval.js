1211080349e8f93e136dccec6cf8b1666bc5d872
ctotone <ctotone@gmail.com>
2023-03-17 13:47:15 +0100

    Create FX Cheval.js

---
+++ Deadlands/Macros/FX Cheval.js
@@ -0,0 +1,18 @@
+?{Choix du son|
+,|
+----- Cheval -----,
+|
+Hénissement n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval hénissement
+|
+Hénissement n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval hennissement 1
+|
+Hénissement n°3,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval hennissement 2
+|
+Hénissement n°4,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval hennissement 3
+|
+Cheval au pas,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval au pas
+|
+Cheval au galop n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval galop (sur terre)
+|
+Cheval au galop n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cheval galop boucle
+}
