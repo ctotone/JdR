47025876473afa64b68564edbbae4d583a15dac7
ctotone <ctotone@gmail.com>
2023-03-17 13:55:46 +0100

    Create FX Jesus.js

---
+++ Deadlands/Macros/FX Jesus.js
@@ -0,0 +1,18 @@
+?{Choix du son|
+,|
+----- Hallelujah -----,
+|
+Cloche n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cloches 1
+|
+Cloche n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Cloches 2
+|
+Magie n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Magie 1
+|
+Magie n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Magie 2
+|
+Magie n°3,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Magie 3
+|
+Magie n°4,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Magie 4
+|
+Magie n°5,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Magie 5
+}
