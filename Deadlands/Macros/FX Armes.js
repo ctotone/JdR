18b1aabef981c90130663a64fe2345db0d3873ab
ctotone <ctotone@gmail.com>
2023-03-17 13:49:32 +0100

    Create FX Armes.js

---
+++ Deadlands/Macros/FX Armes.js
@@ -0,0 +1,20 @@
+?{Choix du son|
+,|
+----- Armes -----,
+|
+Pistolet n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pistolet
+|
+Pistolet n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pistolet1
+|
+Pistolet n°3,!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pistolet2
+|
+Pistolet n°4,!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pistolet3
+|
+Fusil à pompe,!roll20AM --audio&#44;play&#44;nomenu&#124;GUN-Pompe
+|
+Arme de chasse,!roll20AM --audio&#44;play&#44;nomenu&#124;GUN - Sniper
+|
+Coup de poing n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Coup de poing
+|
+Coup de poing n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Coup de poing1
+}
