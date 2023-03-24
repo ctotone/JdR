7fc9094f89f2db1f29c9b4fc08879c4821e4a057
ctotone <ctotone@gmail.com>
2023-03-24 20:25:07 +0100

    maj

---
+++ Deadlands/Macros/FX Tonerre.js
@@ -0,0 +1,14 @@
+?{Choix du son|
+,|
+----- Tonerre -----,
+|
+Tonerre n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Tonerre 1
+|
+Tonerre n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Tonerre 2
+|
+Tonerre n°3,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Tonerre 3
+|
+Tonerre n°4,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Tonerre 4
+|
+Tonerre n°5,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Tonerre 5
+}
