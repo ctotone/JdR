cbae338192a0446b2f3ca6b9b48d19cbf0e60ed8
ctotone <ctotone@gmail.com>
2023-04-18 23:29:42 +0200

    merge

---
+++ Deadlands/Macros/FX Canon.js
@@ -0,0 +1,17 @@
+?{Choix du son|
+,|
+----- Canon -----,
+|
+Canon n°1,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Canon (1)
+|
+Canon n°2,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Canon (2)
+|
+Canon n°3,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Canon (3)
+|
+Canon n°4,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Canon (4)
+|
+Canon n°5,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Canon (5)
+|
+Canon n°6,!roll20AM --audio&#44;play&#44;nomenu&#124;FX - Canon (6)
+
+}
