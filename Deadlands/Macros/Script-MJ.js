54a58c3f41b4e8fc6d209cead8d36a0cca3fa1c8
ctotone <ctotone@gmail.com>
2023-04-21 14:53:08 +0200

    Update and rename Turn Order.js to Script-MJ.js

---
+++ Deadlands/Macros/Script-MJ.js
@@ -0,0 +1,23 @@
+// Macro pour les commandes MJ
+
+
+?{Commande MJ|
+,|
+----- Turn-Order -----,
+,|
+Mélanger,!deal-init --reset --quiet
+|
+Nouveau Round,!deal-init
+|
+Tirer une carte,!deal-init --deal2chat
+|
+Fin Combat,!deal-init --clearTurnOrder
+|
+----- Calendrier -----,
+|
+Menu,!cal
+|
+Config,!settings
+|
+Afficher calendrier,!playercal
+}
