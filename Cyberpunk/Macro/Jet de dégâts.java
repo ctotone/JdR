5f79e315570b6360d9cf258763f2f919ec268a6e
ctotone <ctotone@gmail.com>
2023-09-05 17:11:23 +0200

    Jet de dégat sans selected
    Pour permettre de faire fonctionner le link de la macro des combats

--- Cyberpunk/Macro/Jet de dégâts.java
+++ Cyberpunk/Macro/Jet de dégâts.java
@@ -1,39 +1,49 @@
-?{Nombre de dés de dégâts|1d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+?{Nombre de dés de dégâts|1d6,  
+L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible
 
 et inflige [[1d6]] dégâts.
-|2d6, **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+|2d6, 
+L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible
 
 et inflige [[2d6]] dégâts.
 /e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
-|3d6,**@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+|3d6,
+L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible
 
 et inflige [[3d6]] dégâts.
 /e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
-|4d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+|4d6,  
+L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible
 
 et inflige [[4d6]] dégâts.
 /e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
-|5d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+|5d6,  
+L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible
 
 et inflige [[5d6]] dégâts.
 /e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
-|6d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+|6d6,  
+L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible
 
 et inflige [[6d6]] dégâts.
 /e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
-|7d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+|7d6,  
+L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible
 
 et inflige [[7d6]] dégâts.
 /e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
-|8d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+|8d6,  
+L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible
 
 et inflige [[8d6]] dégâts.
 /e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
-|9d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+|9d6,  
+L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible
 
 et inflige [[9d6]] dégâts.
 /e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
-|10d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+|10d6,  
+L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible
 
 et inflige [[10d6]] dégâts.
 /e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)}
