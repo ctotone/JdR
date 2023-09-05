e6c1bcf333f2e382f728a1cf618a63cabb7e34dd
ctotone <ctotone@gmail.com>
2023-09-05 16:00:48 +0200

    Ajout localisation dans macro dégats

--- Cyberpunk/Macro/Jet de dégâts.java
+++ Cyberpunk/Macro/Jet de dégâts.java
@@ -1,48 +1,39 @@
-?{Nombre de dés de dégâts|1d6,  **@{selected|character_name}** inflige [[1d6]] dégâts.
-/w gm Lancer macro Localisation
-(Réussite critique et SD +10 ?)
-|2d6, **@{selected|character_name}** inflige [[2d6]] dégâts.
-**Blessure critique !** si double 6
-et Réussite critique et SD +10 = **Headshot !!**
-/w gm (Blessure CRIT même si dégâts inf. à PA)
-/w gm Lancer macro Localisation
-|3d6,**@{selected|character_name}** inflige [[3d6]] dégâts.
-**Blessure critique !** si double 6
-et Réussite critique et SD +10 = **Headshot !!**
-/w gm Lancer macro Localisation
-/w gm Blessure CRIT même si dégâts inf. à PA
-|4d6,  **@{selected|character_name}** inflige [[4d6]] dégâts.
-**Blessure critique !** si double 6
-et Réussite critique et SD +10 = **Headshot !!**
-/w gm Lancer macro Localisation
-/w gm Blessure CRIT même si dégâts inf. à PA
-|5d6,  **@{selected|character_name}** inflige [[5d6]] dégâts.
-**Blessure critique !** si double 6
-et Réussite critique et SD +10 = **Headshot !!**
-/w gm Lancer macro Localisation
-/w gm Blessure CRIT même si dégâts inf. à PA
-|6d6,  **@{selected|character_name}** inflige [[6d6]] dégâts.
-**Blessure critique !** si double 6
-et Réussite critique et SD +10 = **Headshot !!**
-/w gm Lancer macro Localisation
-/w gm Blessure CRIT même si dégâts inf. à PA
-|7d6,  **@{selected|character_name}** inflige [[7d6]] dégâts.
-**Blessure critique !** si double 6
-et Réussite critique et SD +10 = **Headshot !!**
-/w gm Lancer macro Localisation
-/w gm Blessure CRIT même si dégâts inf. à PA
-|8d6,  **@{selected|character_name}** inflige [[8d6]] dégâts.
-**Blessure critique !** si double 6
-et Réussite critique et SD +10 = **Headshot !!**
-/w gm Lancer macro Localisation
-/w gm Blessure CRIT même si dégâts inf. à PA
-|9d6,  **@{selected|character_name}** inflige [[9d6]] dégâts.
-**Blessure critique !** si double 6
-et Réussite critique et SD +10 = **Headshot !!**
-/w gm Lancer macro Localisation
-/w gm Blessure CRIT même si dégâts inf. à PA
-|10d6,  **@{selected|character_name}** inflige [[10d6]] dégâts.
-**Blessure critique !** si double 6
-et Réussite critique et SD +10 = **Headshot !!**
-/w gm Lancer macro Localisation
-/w gm Blessure CRIT même si dégâts inf. à PA}
+?{Nombre de dés de dégâts|1d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+
+et inflige [[1d6]] dégâts.
+|2d6, **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+
+et inflige [[2d6]] dégâts.
+/e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
+|3d6,**@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+
+et inflige [[3d6]] dégâts.
+/e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
+|4d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+
+et inflige [[4d6]] dégâts.
+/e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
+|5d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+
+et inflige [[5d6]] dégâts.
+/e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
+|6d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+
+et inflige [[6d6]] dégâts.
+/e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
+|7d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+
+et inflige [[7d6]] dégâts.
+/e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
+|8d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+
+et inflige [[8d6]] dégâts.
+/e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
+|9d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+
+et inflige [[9d6]] dégâts.
+/e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)
+|10d6,  **@{selected|character_name}** impacte [[1t[Localisation-dégâts]]] de sa cible
+
+et inflige [[10d6]] dégâts.
+/e **Blessure critique !** si double 6 (même si les dégâts sont inférieurs au PA de la cible)}
