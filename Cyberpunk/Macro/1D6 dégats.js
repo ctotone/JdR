6cbd4f33008940eb784c01b3bbeb9a0d9c6f8f90
ctotone <ctotone@gmail.com>
2023-09-05 18:25:01 +0200

    Create 1D6 dégats.js

---
+++ Cyberpunk/Macro/1D6 dégats.js
@@ -0,0 +1,6 @@
+
+L'attaque impacte [[1t[Localisation-dégâts]]] de @{target|character_name} 
+
+et inflige [[1d6]] dégâts.
+
+/w gm Armure de @{target|character_name} : Corps @{target|BodySP} | Tête @{target|HeadSP}
