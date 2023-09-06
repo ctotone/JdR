085d0198038899812e340f509ce8d9ca4a23581d
ctotone <ctotone@gmail.com>
2023-09-06 16:04:49 +0200

    Update aide.js

--- Cyberpunk/Macro/aide.js
+++ Cyberpunk/Macro/aide.js
@@ -1,10 +1,10 @@
 ?{Choix de l'aide|
 ,|
 SD des actions,
-&{template:default&#125;{{name=SD des actions&#125;&#125;{{Simple=9&#125;&#125;{{Facile=13&#125;&#125;{{Moyen=15&#125;&#125;{{Professionel=17&#125;&#125;{{Assez difficile=21&#125;&#125;{{Difficile=24&#125;&#125;{{Presque impossible=29&#125;&#125;
+&{template:custom&#125;{{title=SD des actions&#125;&#125;{{Simple=9&#125;&#125;{{Facile=13&#125;&#125;{{Moyen=15&#125;&#125;{{Professionel=17&#125;&#125;{{Assez difficile=21&#125;&#125;{{Difficile=24&#125;&#125;{{Presque impossible=29&#125;&#125;
 ,|
 Seuil de blessure,
-&{template:default&#125;{{name=Blessure&#125;&#125;{{Légère=Aucun malus&#125;&#125;{{Grave=Inférieur à 50% HP.&#125;&#125; {{&nbsp;=-2 à toutes les actions&#125;&#125;{{Mortelle=Inférieur à 1HP.&#125;&#125;{{&nbsp;&nbsp;&nbsp;=-4 à toutes les actions et -6 au MOUV.&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;=Jet de sauvegarde contre la mort au début de chaque tour&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=En cas de blessure mortelle&#44; le personnage subit une blessure critique chaque fois qu'il subit des dégats d'une attaque.&#125;&#125;
+&{template:custom&#125;{{title=Blessure&#125;&#125;{{Légère=Aucun malus&#125;&#125;{{Grave=Inférieur à 50% HP.&#125;&#125; {{&nbsp;=-2 à toutes les actions&#125;&#125;{{Mortelle=Inférieur à 1HP.&#125;&#125;{{&nbsp;&nbsp;&nbsp;=-4 à toutes les actions et -6 au MOUV.&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;=Jet de sauvegarde contre la mort au début de chaque tour&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=En cas de blessure mortelle&#44; le personnage subit une blessure critique chaque fois qu'il subit des dégats d'une attaque.&#125;&#125;
 ,|
 titre,
 &{template:custom&#125;{{title=titre&#125;&#125;{{1=blabla&#125;&#125;{{2=blabla2&#125;&#125;
