fe25769c71eece1f0afd55c98948baa2b63a95d2
ctotone <ctotone@gmail.com>
2023-09-07 13:35:14 +0200

    Update aide.js

--- Cyberpunk/Macro/aide.js
+++ Cyberpunk/Macro/aide.js
@@ -6,8 +6,11 @@ SD des actions,
 Seuil de blessure,
 &{template:custom&#125;{{title=Blessure&#125;&#125;{{Légère=Aucun malus&#125;&#125;{{Grave=Inférieur à 50% HP.&#125;&#125; {{&nbsp;=-2 à toutes les actions&#125;&#125;{{Mortelle=Inférieur à 1HP.&#125;&#125;{{&nbsp;&nbsp;&nbsp;=-4 à toutes les actions et -6 au MOUV.&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;=Jet de sauvegarde contre la mort au début de chaque tour&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=En cas de blessure mortelle&#44; le personnage subit une blessure critique chaque fois qu'il subit des dégats d'une attaque.&#125;&#125;
 ,|
-titre,
-&{template:custom&#125;{{title=titre&#125;&#125;{{1=blabla&#125;&#125;{{2=blabla2&#125;&#125;
+Viser,
+&{template:custom&#125;{{title=Viser une partie d'un corps ou d'un objet est possible avec un malus de -5 sur l'action.&#125;&#125;{{Améliorer la visée=Chaque round passé à viser réduit le malus de 1.&#125;&#125;{{Viser la tête=Multiplie par 2 les dégâts qui traversent l'armure de tête de la cible.&#125;&#125;{{Viser une jambe=Si ne serait-ce qu'un seul point de dégâts traverse l'armure&#44; la cible subit également la blessure critique jambe cassée.&#125;&#125;{{Viser un objet tenu=Si ne serait-ce qu'un seul point de dégâts traverse l'armure&#44; la cible lâche un objet au choix qu'elle tenait dans la main.&#125;&#125;
+,|
+Corps à corps,
+&{template:custom&#125;{{title=Le corps à corps englobe les armes de mélée et le combat à mains nues .&#125;&#125;{{SD de l'attaque=Il s'agit du résultat du jet d'Esquive de la cible.&#125;&#125;{{&nbsp;=&#125;&#125;{{Armes de mêlée=Ignore la moitié de l'armure de la cible en cas de réussite de l'attaque.&#125;&#125;{{Arme de mêlée légère=Couteau&#44; tomahawk.&#125;&#125;{{Arme de mêlée inter.=Batte&#44; pied de biche&#44; machette.&#125;&#125;{{Arme de mêlée lourde=Tuyau de plomb&#44; épée&#44; batte à pointes.&#125;&#125;{{Arme de mêlée très lourde=Tronçonneuse&#44; masse&#44; pales d'hélicoptère&#44; naginata.&#125;&#125;{{&nbsp;&nbsp;=&#125;&#125;{{Combat à mains nues=N'ignore pas la moitié de l'armure de la cible en cas de réussite de l'attaque.&#125;&#125;
 ,
 
 }
