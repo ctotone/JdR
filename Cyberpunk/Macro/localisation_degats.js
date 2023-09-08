86c065b95fa955a4a2f807374eb6933ceaa134c5
ctotone <ctotone@gmail.com>
2023-09-08 13:31:16 +0200

    Create localisation_degats.js

---
+++ Cyberpunk/Macro/localisation_degats.js
@@ -0,0 +1,4 @@
+?{Localisation blessure critique|
+Corps, &{template:custom&#125;{{title=**Blessure critique au corps**&#125;&#125;{{color=red&#125;&#125;{{desc=[[1t[critique-corps]]]&#125;&#125;|
+Tête, &{template:custom&#125;{{title=Blessure critique à la tête&#125;&#125;{{=[[1t[critique-tête]]]&#125;&#125;
+}
