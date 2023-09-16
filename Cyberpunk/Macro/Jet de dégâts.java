bbc23323fe87731a49d129fcffa0f82b42c17e9a
ctotone <ctotone@gmail.com>
2023-09-16 19:36:36 +0200

    merge

--- Cyberpunk/Macro/Jet de dégâts.java
+++ Cyberpunk/Macro/Jet de dégâts.java
@@ -1,77 +1,58 @@
 ?{Nombre de dés de dégâts|1d6,  
 &{template:custom&#125;{{title=&#125;&#125;{{color=red&#125;&#125;{{desc=L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible et inflige [[2d6]] dégâts.
-
-Armure de @{target|character_name} : Corps @{target|BodySP} &#124; Tête @{target|HeadSP}&#125;&#125;
+&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés 2
 |2d6, 
 &{template:custom&#125;{{title=&#125;&#125;{{color=red&#125;&#125;{{desc=L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible et inflige [[2d6]] dégâts.
 
 **Blessure critique !** si double 6 
-(même si les dégâts sont inférieurs au PA de la cible)
-
-Armure de @{target|character_name} : Corps @{target|BodySP} &#124; Tête @{target|HeadSP}&#125;&#125;
+(même si les dégâts sont inférieurs au PA de la cible)&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés 2
 |3d6,
 &{template:custom&#125;{{title=&#125;&#125;{{color=red&#125;&#125;{{desc=L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible et inflige [[3d6]] dégâts.
 
 **Blessure critique !** si double 6 
-(même si les dégâts sont inférieurs au PA de la cible)
-
-Armure de @{target|character_name} : Corps @{target|BodySP} &#124; Tête @{target|HeadSP}&#125;&#125;
+(même si les dégâts sont inférieurs au PA de la cible)&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés 2
 |4d6,  
 &{template:custom&#125;{{title=&#125;&#125;{{color=red&#125;&#125;{{desc=L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible et inflige [[4d6]] dégâts.
 
 **Blessure critique !** si double 6 
-(même si les dégâts sont inférieurs au PA de la cible)
-
-Armure de @{target|character_name} : Corps @{target|BodySP} &#124; Tête @{target|HeadSP}&#125;&#125;
+(même si les dégâts sont inférieurs au PA de la cible)&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés 2
 |5d6,  
 &{template:custom&#125;{{title=&#125;&#125;{{color=red&#125;&#125;{{desc=L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible et inflige [[5d6]] dégâts.
 
 **Blessure critique !** si double 6 
-(même si les dégâts sont inférieurs au PA de la cible)
-
-Armure de @{target|character_name} : Corps @{target|BodySP} &#124; Tête @{target|HeadSP}&#125;&#125;
+(même si les dégâts sont inférieurs au PA de la cible)&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés 2
 |6d6,  
 &{template:custom&#125;{{title=&#125;&#125;{{color=red&#125;&#125;{{desc=L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible et inflige [[6d6]] dégâts.
 
 **Blessure critique !** si double 6 
-(même si les dégâts sont inférieurs au PA de la cible)
-
-Armure de @{target|character_name} : Corps @{target|BodySP} &#124; Tête @{target|HeadSP}&#125;&#125;
+(même si les dégâts sont inférieurs au PA de la cible)&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés 2
 |7d6,  
 &{template:custom&#125;{{title=&#125;&#125;{{color=red&#125;&#125;{{desc=L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible et inflige [[7d6]] dégâts.
 
 **Blessure critique !** si double 6 
-(même si les dégâts sont inférieurs au PA de la cible)
-
-Armure de @{target|character_name} : Corps @{target|BodySP} &#124; Tête @{target|HeadSP}&#125;&#125;
+(même si les dégâts sont inférieurs au PA de la cible)&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés 2
 |8d6,  
 &{template:custom&#125;{{title=&#125;&#125;{{color=red&#125;&#125;{{desc=L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible et inflige [[8d6]] dégâts.
 
 **Blessure critique !** si double 6 
-(même si les dégâts sont inférieurs au PA de la cible)
-
-Armure de @{target|character_name} : Corps @{target|BodySP} &#124; Tête @{target|HeadSP}&#125;&#125;
+(même si les dégâts sont inférieurs au PA de la cible)&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés 2
 |9d6,  
 &{template:custom&#125;{{title=&#125;&#125;{{color=red&#125;&#125;{{desc=L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible et inflige [[9d6]] dégâts.
 
 **Blessure critique !** si double 6 
-(même si les dégâts sont inférieurs au PA de la cible)
-
-Armure de @{target|character_name} : Corps @{target|BodySP} &#124; Tête @{target|HeadSP}&#125;&#125;
+(même si les dégâts sont inférieurs au PA de la cible)&#125;&#125;
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés 2
 |10d6,  
 &{template:custom&#125;{{title=&#125;&#125;{{color=red&#125;&#125;{{desc=L'attaque impacte [[1t[Localisation-dégâts]]] de sa cible et inflige [[10d6]] dégâts.
 
 **Blessure critique !** si double 6 
-(même si les dégâts sont inférieurs au PA de la cible)
-
-Armure de @{target|character_name} : Corps @{target|BodySP} &#124; Tête @{target|HeadSP}&#125;&#125;}
+(même si les dégâts sont inférieurs au PA de la cible)&#125;&#125;}
 !roll20AM --audio&#44;play&#44;nomenu&#124;FX - Jet de dés 2
---
+++ Cyberpunk/Macro/critique.js
@@ -0,0 +1,47 @@
+?{Localisation blessure critique|
+Torse,!power {{
+--format&#124;cyber
+--orowbg&#124;#FFFFFF
+--orowtx&#124;#000000
+--erowbg&#124;#FFFFFF
+--erowtx&#124;#000000
+--titlefontsize&#124;13px
+--bodyfontsize&#124;12px
+--border&#124;1px solid #f00
+--name&#124;Blessure critique au torse
+--&#124;[[1t[critique-torse]]]&#125;&#125;
+|
+Bras,!power {{
+--format&#124;cyber
+--orowbg&#124;#FFFFFF
+--orowtx&#124;#000000
+--erowbg&#124;#FFFFFF
+--erowtx&#124;#000000
+--titlefontsize&#124;13px
+--bodyfontsize&#124;12px
+--border&#124;1px solid #f00
+--name&#124;Blessure critique au bras
+--&#124;[[1t[critique-bras]]]&#125;&#125;|
+Jambe,!power {{
+--format&#124;cyber
+--orowbg&#124;#FFFFFF
+--orowtx&#124;#000000
+--erowbg&#124;#FFFFFF
+--erowtx&#124;#000000
+--titlefontsize&#124;13px
+--bodyfontsize&#124;12px
+--border&#124;1px solid #f00
+--name&#124;Blessure critique à la jambe
+--&#124;[[1t[critique-jambe]]]&#125;&#125;|
+Tête,!power {{
+--format&#124;cyber
+--orowbg&#124;#FFFFFF
+--orowtx&#124;#000000
+--erowbg&#124;#FFFFFF
+--erowtx&#124;#000000
+--titlefontsize&#124;13px
+--bodyfontsize&#124;12px
+--border&#124;1px solid #f00
+--name&#124;Blessure critique à la tête
+--&#124;[[1t[critique-tête]]]&#125;&#125;
+}
