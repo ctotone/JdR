61bdbbe9e8edf79c0f6d71d01984a13b9d161573
ctotone <ctotone@gmail.com>
2023-09-19 18:06:58 +0200

    Create GM-Tool.js

---
+++ Cyberpunk/Macro/GM-Tool.js
@@ -0,0 +1,9 @@
+/w gm &{template:default} {{name=}}{{=**HP aura**
+[Menu](!aura) | [ON/OFF](!aura npc)
+**TurnMarker**
+[Menu](!tm)
+**Token Mod**
+[NOM ON](!token-mod --on showname --report gm|'Nom affiché') | [NOM OFF](!token-mod --off showname --report gm|'Nom caché')
+[TOKEN DEFAUT](!token-mod --set defaulttoken --report gm|'Token par défaut mis à jour')
+[TOKEN PREPA PNJ](! #*GMPrepapnjmaccro)
+}}
