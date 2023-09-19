fcfe1af1301f9ed53cfec29fa0b2131ed560b8f5
ctotone <ctotone@gmail.com>
2023-09-19 18:41:06 +0200

    Create Rencontre aléatoire.js

---
+++ Cyberpunk/Macro/Rencontre aléatoire.js
@@ -0,0 +1,54 @@
+?{Type de rencontre|
+Zone de reconstruction - Diurne,  
+!power {{
+--whisper&#124;GM
+--format&#124;cyber
+--orowbg&#124;#FFFFFF
+--orowtx&#124;#000000
+--erowbg&#124;#FFFFFF
+--erowtx&#124;#000000
+--titlefontsize&#124;13px
+--bodyfontsize&#124;12px
+--border&#124;1px solid #f00
+--name&#124;Rencontre aléatoire
+--&#124;[[1t[rencontre-diurne-zone-reconstruction]]]&#125;&#125;
+|Zone de combat - Diurne,   
+!power {{
+--whisper&#124;GM
+--format&#124;cyber
+--orowbg&#124;#FFFFFF
+--orowtx&#124;#000000
+--erowbg&#124;#FFFFFF
+--erowtx&#124;#000000
+--titlefontsize&#124;13px
+--bodyfontsize&#124;12px
+--border&#124;1px solid #f00
+--name&#124;Rencontre aléatoire
+--&#124;[[1t[rencontre-diurne-zone-combat]]]&#125;&#125;
+|Zone de reconstruction - Nocturne,  
+!power {{
+--whisper&#124;GM
+--format&#124;cyber
+--orowbg&#124;#FFFFFF
+--orowtx&#124;#000000
+--erowbg&#124;#FFFFFF
+--erowtx&#124;#000000
+--titlefontsize&#124;13px
+--bodyfontsize&#124;12px
+--border&#124;1px solid #f00
+--name&#124;Rencontre aléatoire
+--&#124;[[1t[rencontre-nocturne-zone-reconstruction]]]&#125;&#125;
+|Zone de combat - Nocturne,    
+!power {{
+--whisper&#124;GM
+--format&#124;cyber
+--orowbg&#124;#FFFFFF
+--orowtx&#124;#000000
+--erowbg&#124;#FFFFFF
+--erowtx&#124;#000000
+--titlefontsize&#124;13px
+--bodyfontsize&#124;12px
+--border&#124;1px solid #f00
+--name&#124;Rencontre aléatoire
+--&#124;[[1t[rencontre-nocturne-zone-combat]]]&#125;&#125;
+}
