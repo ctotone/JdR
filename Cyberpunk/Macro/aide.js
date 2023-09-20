edc546bfbe5b32f0c8b4988243f3ff868aae14e6
ctotone <ctotone@gmail.com>
2023-09-20 13:25:28 +0200

    Update aide.js

--- Cyberpunk/Macro/aide.js
+++ Cyberpunk/Macro/aide.js
@@ -20,12 +20,15 @@ L'armure n'est pas prise en compte.&#125;&#125;{{Chute libre=Inflige 2D6 de dég
 
 Test Athlésme SD15 et en cas d'échec Blessure critique jambe.&#125;&#125;{{Poison et drogues=Info page 181 &#125;&#125;
 ,|
+Manoeuvres de véhicule,/w gm &{template:custom&#125;{{title=Manoeuvres&#125;&#125;{{color=noir&#125;&#125;{{Embardée=SD13&#125;&#125;{{Virage brutal=SD13&#125;&#125;{{Arrêt d'urgence=SD13&#125;&#125;{{Virage à 180=SD17&#125;&#125;{{Saut=SD17&#125;&#125;{{Atterissage 
+(aéronef)=SD13&#125;&#125;{{Acrobatie 
+(aéronef)=SD17&#125;&#125;{{Charger=Il est possible de charger avec le véhicule pour infliger 6D6 de dégâts à la cible ET à votre véhicule.&#125;&#125;
+,|
 ----- JOUEUR -----,
 ,|
 SD des actions,
 &{template:custom&#125;{{title=SD des actions&#125;&#125;{{color=noir&#125;&#125;{{Simple=9&#125;&#125;{{Facile=13&#125;&#125;{{Moyen=15&#125;&#125;{{Professionel=17&#125;&#125;{{Assez difficile=21&#125;&#125;{{Difficile=24&#125;&#125;{{Presque impossible=29&#125;&#125;
 ,|
-
 Seuil de blessure,
 &{template:custom&#125;{{title=Blessure&#125;&#125;{{color=noir&#125;&#125;{{Légère=Aucun malus&#125;&#125;{{Grave=Inférieur à 50% HP.&#125;&#125; {{&nbsp;=-2 à toutes les actions&#125;&#125;{{Mortelle=Inférieur à 1HP.&#125;&#125;{{&nbsp;&nbsp;&nbsp;=-4 à toutes les actions et -6 au MOUV.&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;=Jet de sauvegarde contre la mort au début de chaque tour&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=En cas de blessure mortelle&#44; le personnage subit une blessure critique chaque fois qu'il subit des dégats d'une attaque.&#125;&#125;
 ,|
@@ -46,6 +49,11 @@ Test Athlésme SD15 et en cas d'échec Blessure critique jambe.&#125;&#125;{{Poi
 ,|
 Actions de combat,
 &{template:custom&#125;{{title=Actions de combat&#125;&#125;{{color=noir&#125;&#125;{{Action de mouvement=Se déplacer de votre MOUV x 2 maximum chaque tour.&#125;&#125;{{Agripper=Agripper un adversaire et le retenir ou s'emparer d'un objet qu'il tient.&#125;&#125;{{Attaquer=Attaquer avec une arme à distance ou de mêlée.&#125;&#125;{{Courir=Effectuer une action de mouvement supplémentaire.&#125;&#125;{{Démarrer un véhicule=Démarrer un véhicule.&#125;&#125;{{Lancer=Projeter au sol un adversaire agrippé ou lancer un objet.&#125;&#125;{{Monter dans un véhicule=Monter dans un véhicule.&#125;&#125;{{Retarder son action=Retarder son action pour la réaliser plus tard après avoir annoncé le declencheur au MJ.&#125;&#125;{{Se relever=Se relever quand on est au sol.&#125;&#125;{{Stabiliser=Stabiliser une cible pour entammer le processus de guerison naturelle.&#125;&#125;{{Actions virtuelles=Netrunner&#125;&#125;{{Utiliser une compétence=Utiliser une compétence pour accomplir une tâche rapide.&#125;&#125;{{Utiliser un objet=Utiliser un objet sans recourir à une compétence&#125;&#125;
+,|
+Manoeuvres de véhicule,
+&{template:custom&#125;{{title=Manoeuvres&#125;&#125;{{color=noir&#125;&#125;{{Embardée=SD13&#125;&#125;{{Virage brutal=SD13&#125;&#125;{{Arrêt d'urgence=SD13&#125;&#125;{{Virage à 180=SD17&#125;&#125;{{Saut=SD17&#125;&#125;{{Atterissage 
+(aéronef)=SD13&#125;&#125;{{Acrobatie 
+(aéronef)=SD17&#125;&#125;{{Charger=Il est possible de charger avec le véhicule pour infliger 6D6 de dégâts à la cible ET à votre véhicule.&#125;&#125;
 ,
 
 }
