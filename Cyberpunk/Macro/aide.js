e2259d97a223ca8de8915c0d0d090d8c41a57072
ctotone <ctotone@gmail.com>
2023-09-20 13:07:05 +0200

    Update aide.js

--- Cyberpunk/Macro/aide.js
+++ Cyberpunk/Macro/aide.js
@@ -1,24 +1,39 @@
 ?{Choix de l'aide|
 ,|
+------ GM ------,
+,|
+SD des actions,/w gm &{template:custom&#125;{{title=SD des actions&#125;&#125;{{color=noir&#125;&#125;{{Simple=9&#125;&#125;{{Facile=13&#125;&#125;{{Moyen=15&#125;&#125;{{Professionel=17&#125;&#125;{{Assez difficile=21&#125;&#125;{{Difficile=24&#125;&#125;{{Presque impossible=29&#125;&#125;
+,|
+Seuil de blessure,/w gm &{template:custom&#125;{{title=Blessure&#125;&#125;{{color=noir&#125;&#125;{{Légère=Aucun malus&#125;&#125;{{Grave=Inférieur à 50% HP.&#125;&#125; {{&nbsp;=-2 à toutes les actions&#125;&#125;{{Mortelle=Inférieur à 1HP.&#125;&#125;{{&nbsp;&nbsp;&nbsp;=-4 à toutes les actions et -6 au MOUV.&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;=Jet de sauvegarde contre la mort au début de chaque tour&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=En cas de blessure mortelle&#44; le personnage subit une blessure critique chaque fois qu'il subit des dégats d'une attaque.&#125;&#125;
+,|
+Viser,/w gm &{template:custom&#125;{{title=Viser une partie d'un corps ou d'un objet est possible avec un malus de -5 sur l'action.&#125;&#125;{{color=noir&#125;&#125;{{Améliorer la visée=Chaque round passé à viser réduit le malus de 1.&#125;&#125;{{Viser la tête=Multiplie par 2 les dégâts qui traversent l'armure de tête de la cible.&#125;&#125;{{Viser une jambe=Si ne serait-ce qu'un seul point de dégâts traverse l'armure&#44; la cible subit également la blessure critique jambe cassée.&#125;&#125;{{Viser un objet tenu=Si ne serait-ce qu'un seul point de dégâts traverse l'armure&#44; la cible lâche l'objet visé qu'elle tenait dans la main.&#125;&#125;
+,|
+Corps à corps,/w gm &{template:custom&#125;{{title=Le corps à corps englobe les armes de mélée et le combat à mains nues .&#125;&#125;{{color=noir&#125;&#125;{{SD de l'attaque=Il s'agit du résultat du jet d'Esquive de la cible.&#125;&#125;{{&nbsp;=&#125;&#125;{{Armes de mêlée=Ignore la moitié de l'armure de la cible en cas de réussite de l'attaque.&#125;&#125;{{Arme de mêlée légère=Couteau&#44; tomahawk.&#125;&#125;{{Arme de mêlée inter.=Batte&#44; pied de biche&#44; machette.&#125;&#125;{{Arme de mêlée lourde=Tuyau de plomb&#44; épée&#44; batte à pointes.&#125;&#125;{{Arme de mêlée très lourde=Tronçonneuse&#44; masse&#44; pales d'hélicoptère&#44; naginata.&#125;&#125;{{&nbsp;&nbsp;=&#125;&#125;{{Combat à mains nues=N'ignore pas la moitié de l'armure de la cible en cas de réussite de l'attaque.&#125;&#125;{{Agripper=Jet de **Bagarre** opposé. Cela permet d'étrangler ou projetter au sol.&#125;&#125;{{Étrangler=Vous infliger vos dégâts de COR et l'armure est ignorée. 3 round d'étranglement rend la cible inconsciente.&#125;&#125;{{Lancer au sol=Vous infliger vos dégâts de COR et l'armure est ignorée.&#125;&#125;
+,|
+Actions de combat,/w gm &{template:custom&#125;{{title=Actions de combat&#125;&#125;{{color=noir&#125;&#125;{{Action de mouvement=Se déplacer de votre MOUV x 2 maximum chaque tour.&#125;&#125;{{Agripper=Agripper un adversaire et le retenir ou s'emparer d'un objet qu'il tient.&#125;&#125;{{Attaquer=Attaquer avec une arme à distance ou de mêlée.&#125;&#125;{{Courir=Effectuer une action de mouvement supplémentaire.&#125;&#125;{{Démarrer un véhicule=Démarrer un véhicule.&#125;&#125;{{Lancer=Projeter au sol un adversaire agrippé ou lancer un objet.&#125;&#125;{{Monter dans un véhicule=Monter dans un véhicule.&#125;&#125;{{Retarder son action=Retarder son action pour la réaliser plus tard après avoir annoncé le declencheur au MJ.&#125;&#125;{{Se relever=Se relever quand on est au sol.&#125;&#125;{{Stabiliser=Stabiliser une cible pour entammer le processus de guerison naturelle.&#125;&#125;{{Actions virtuelles=Netrunner&#125;&#125;{{Utiliser une compétence=Utiliser une compétence pour accomplir une tâche rapide.&#125;&#125;{{Utiliser un objet=Utiliser un objet sans recourir à une compétence&#125;&#125;
+,|
+Poison&#44; noyade&#44; etc...,/w gm &{template:custom&#125;{{title=Dégâts&#125;&#125;{{color=noir&#125;&#125;{{Electrocution=Inflige 6D6 de dégâts chaque tour&#44; l'armure est prise en compte.&#125;&#125;{{Feu=- Intensité faible (2PS)
+- Intensité Forte (4PS)
+- Intensité Mortelle (6PS)
+
+L'armure n'est pas prise en compte.&#125;&#125;{{Chute libre=Inflige 2D6 de dégats par tranche de 10m&#44; l'armure est prise en compte.
+
+Test Athlésme SD15 et en cas d'échec Blessure critique jambe.&#125;&#125;{{Poison et drogues=Info page 181 &#125;&#125;
+,|
+----- JOUEUR -----,
+,|
 SD des actions,
 &{template:custom&#125;{{title=SD des actions&#125;&#125;{{color=noir&#125;&#125;{{Simple=9&#125;&#125;{{Facile=13&#125;&#125;{{Moyen=15&#125;&#125;{{Professionel=17&#125;&#125;{{Assez difficile=21&#125;&#125;{{Difficile=24&#125;&#125;{{Presque impossible=29&#125;&#125;
 ,|
---->(GM),/w gm &{template:custom&#125;{{title=SD des actions&#125;&#125;{{color=noir&#125;&#125;{{Simple=9&#125;&#125;{{Facile=13&#125;&#125;{{Moyen=15&#125;&#125;{{Professionel=17&#125;&#125;{{Assez difficile=21&#125;&#125;{{Difficile=24&#125;&#125;{{Presque impossible=29&#125;&#125;
-,|
+
 Seuil de blessure,
 &{template:custom&#125;{{title=Blessure&#125;&#125;{{color=noir&#125;&#125;{{Légère=Aucun malus&#125;&#125;{{Grave=Inférieur à 50% HP.&#125;&#125; {{&nbsp;=-2 à toutes les actions&#125;&#125;{{Mortelle=Inférieur à 1HP.&#125;&#125;{{&nbsp;&nbsp;&nbsp;=-4 à toutes les actions et -6 au MOUV.&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;=Jet de sauvegarde contre la mort au début de chaque tour&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=En cas de blessure mortelle&#44; le personnage subit une blessure critique chaque fois qu'il subit des dégats d'une attaque.&#125;&#125;
-,|
---->(GM),/w gm &{template:custom&#125;{{title=Blessure&#125;&#125;{{color=noir&#125;&#125;{{Légère=Aucun malus&#125;&#125;{{Grave=Inférieur à 50% HP.&#125;&#125; {{&nbsp;=-2 à toutes les actions&#125;&#125;{{Mortelle=Inférieur à 1HP.&#125;&#125;{{&nbsp;&nbsp;&nbsp;=-4 à toutes les actions et -6 au MOUV.&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;=Jet de sauvegarde contre la mort au début de chaque tour&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=En cas de blessure mortelle&#44; le personnage subit une blessure critique chaque fois qu'il subit des dégats d'une attaque.&#125;&#125;
 ,|
 Viser,
 &{template:custom&#125;{{title=Viser une partie d'un corps ou d'un objet est possible avec un malus de -5 sur l'action.&#125;&#125;{{color=noir&#125;&#125;{{Améliorer la visée=Chaque round passé à viser réduit le malus de 1.&#125;&#125;{{Viser la tête=Multiplie par 2 les dégâts qui traversent l'armure de tête de la cible.&#125;&#125;{{Viser une jambe=Si ne serait-ce qu'un seul point de dégâts traverse l'armure&#44; la cible subit également la blessure critique jambe cassée.&#125;&#125;{{Viser un objet tenu=Si ne serait-ce qu'un seul point de dégâts traverse l'armure&#44; la cible lâche l'objet visé qu'elle tenait dans la main.&#125;&#125;
 ,|
---->(GM),/w gm &{template:custom&#125;{{title=Viser une partie d'un corps ou d'un objet est possible avec un malus de -5 sur l'action.&#125;&#125;{{color=noir&#125;&#125;{{Améliorer la visée=Chaque round passé à viser réduit le malus de 1.&#125;&#125;{{Viser la tête=Multiplie par 2 les dégâts qui traversent l'armure de tête de la cible.&#125;&#125;{{Viser une jambe=Si ne serait-ce qu'un seul point de dégâts traverse l'armure&#44; la cible subit également la blessure critique jambe cassée.&#125;&#125;{{Viser un objet tenu=Si ne serait-ce qu'un seul point de dégâts traverse l'armure&#44; la cible lâche l'objet visé qu'elle tenait dans la main.&#125;&#125;
-,|
 Corps à corps,
 &{template:custom&#125;{{title=Le corps à corps englobe les armes de mélée et le combat à mains nues .&#125;&#125;{{color=noir&#125;&#125;{{SD de l'attaque=Il s'agit du résultat du jet d'Esquive de la cible.&#125;&#125;{{&nbsp;=&#125;&#125;{{Armes de mêlée=Ignore la moitié de l'armure de la cible en cas de réussite de l'attaque.&#125;&#125;{{Arme de mêlée légère=Couteau&#44; tomahawk.&#125;&#125;{{Arme de mêlée inter.=Batte&#44; pied de biche&#44; machette.&#125;&#125;{{Arme de mêlée lourde=Tuyau de plomb&#44; épée&#44; batte à pointes.&#125;&#125;{{Arme de mêlée très lourde=Tronçonneuse&#44; masse&#44; pales d'hélicoptère&#44; naginata.&#125;&#125;{{&nbsp;&nbsp;=&#125;&#125;{{Combat à mains nues=N'ignore pas la moitié de l'armure de la cible en cas de réussite de l'attaque.&#125;&#125;{{Agripper=Jet de **Bagarre** opposé. Cela permet d'étrangler ou projetter au sol.&#125;&#125;{{Étrangler=Vous infliger vos dégâts de COR et l'armure est ignorée. 3 round d'étranglement rend la cible inconsciente.&#125;&#125;{{Lancer au sol=Vous infliger vos dégâts de COR et l'armure est ignorée.&#125;&#125;
-,|
---->(GM),/w gm &{template:custom&#125;{{title=Le corps à corps englobe les armes de mélée et le combat à mains nues .&#125;&#125;{{color=noir&#125;&#125;{{SD de l'attaque=Il s'agit du résultat du jet d'Esquive de la cible.&#125;&#125;{{&nbsp;=&#125;&#125;{{Armes de mêlée=Ignore la moitié de l'armure de la cible en cas de réussite de l'attaque.&#125;&#125;{{Arme de mêlée légère=Couteau&#44; tomahawk.&#125;&#125;{{Arme de mêlée inter.=Batte&#44; pied de biche&#44; machette.&#125;&#125;{{Arme de mêlée lourde=Tuyau de plomb&#44; épée&#44; batte à pointes.&#125;&#125;{{Arme de mêlée très lourde=Tronçonneuse&#44; masse&#44; pales d'hélicoptère&#44; naginata.&#125;&#125;{{&nbsp;&nbsp;=&#125;&#125;{{Combat à mains nues=N'ignore pas la moitié de l'armure de la cible en cas de réussite de l'attaque.&#125;&#125;{{Agripper=Jet de **Bagarre** opposé. Cela permet d'étrangler ou projetter au sol.&#125;&#125;{{Étrangler=Vous infliger vos dégâts de COR et l'armure est ignorée. 3 round d'étranglement rend la cible inconsciente.&#125;&#125;{{Lancer au sol=Vous infliger vos dégâts de COR et l'armure est ignorée.&#125;&#125;
 ,|
 Poison&#44; noyade&#44; etc...,
 &{template:custom&#125;{{title=Dégâts&#125;&#125;{{color=noir&#125;&#125;{{Electrocution=Inflige 6D6 de dégâts chaque tour&#44; l'armure est prise en compte.&#125;&#125;{{Feu=- Intensité faible (2PS)
@@ -29,13 +44,8 @@ L'armure n'est pas prise en compte.&#125;&#125;{{Chute libre=Inflige 2D6 de dég
 
 Test Athlésme SD15 et en cas d'échec Blessure critique jambe.&#125;&#125;{{Poison et drogues=Info page 181 &#125;&#125;
 ,|
---->(GM),/w gm &{template:custom&#125;{{title=Dégâts&#125;&#125;{{color=noir&#125;&#125;{{Electrocution=Inflige 6D6 de dégâts chaque tour&#44; l'armure est prise en compte.&#125;&#125;{{Feu=- Intensité faible (2PS)
-- Intensité Forte (4PS)
-- Intensité Mortelle (6PS)
-
-L'armure n'est pas prise en compte.&#125;&#125;{{Chute libre=Inflige 2D6 de dégats par tranche de 10m&#44; l'armure est prise en compte.
-
-Test Athlésme SD15 et en cas d'échec Blessure critique jambe.&#125;&#125;{{Poison et drogues=Info page 181 &#125;&#125;
+Actions de combat,
+&{template:custom&#125;{{title=Actions de combat&#125;&#125;{{color=noir&#125;&#125;{{Action de mouvement=Se déplacer de votre MOUV x 2 maximum chaque tour.&#125;&#125;{{Agripper=Agripper un adversaire et le retenir ou s'emparer d'un objet qu'il tient.&#125;&#125;{{Attaquer=Attaquer avec une arme à distance ou de mêlée.&#125;&#125;{{Courir=Effectuer une action de mouvement supplémentaire.&#125;&#125;{{Démarrer un véhicule=Démarrer un véhicule.&#125;&#125;{{Lancer=Projeter au sol un adversaire agrippé ou lancer un objet.&#125;&#125;{{Monter dans un véhicule=Monter dans un véhicule.&#125;&#125;{{Retarder son action=Retarder son action pour la réaliser plus tard après avoir annoncé le declencheur au MJ.&#125;&#125;{{Se relever=Se relever quand on est au sol.&#125;&#125;{{Stabiliser=Stabiliser une cible pour entammer le processus de guerison naturelle.&#125;&#125;{{Actions virtuelles=Netrunner&#125;&#125;{{Utiliser une compétence=Utiliser une compétence pour accomplir une tâche rapide.&#125;&#125;{{Utiliser un objet=Utiliser un objet sans recourir à une compétence&#125;&#125;
 ,
 
 }
