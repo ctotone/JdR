0f23be55d4fba1bd9e364463dd6740505b0f02b0
ctotone <ctotone@gmail.com>
2023-09-24 13:47:01 +0200

    Update GM-Tool.js

--- Cyberpunk/Macro/GM-Tool.js
+++ Cyberpunk/Macro/GM-Tool.js
@@ -1,4 +1,6 @@
-/w gm &{template:default} {{name=}}{{=**HP aura**
+/w gm &{template:default} {{name=}}{{=**Balle perdue**
+[Balle 1](!roll20AM --audio,play,nomenu|FX - Balle qui passe 1) | [Balle 2](!roll20AM --audio,play,nomenu|FX - Balle qui passe 2) | [Balle 3](!roll20AM --audio,play,nomenu|FX - Balle qui passe 3)
+**HP aura**
 [Menu](!aura) | [ON/OFF](!aura npc)
 **TurnMarker**
 [Menu](!tm)
@@ -6,4 +8,20 @@
 [NOM ON](!token-mod --on showname --report gm|'Nom affiché') | [NOM OFF](!token-mod --off showname --report gm|'Nom caché')
 [TOKEN DEFAUT](!token-mod --set defaulttoken --report gm|'Token par défaut mis à jour')
 [TOKEN PREPA PNJ](! #*GMPrepapnjmaccro)
+}}
+
+/w gm &{template:default} {{name=}}{{=**Balle perdue**
+[Balle 1](!roll20AM --audio,play,nomenu|FX - Balle qui passe 1) | [Balle 2](!roll20AM --audio,play,nomenu|FX - Balle qui passe 2) | [Balle 3](!roll20AM --audio,play,nomenu|FX - Balle qui passe 3)
+**Tir**
+[Gun 1](!roll20AM --audio,play,nomenu|GUN-Pistolet) | [Gun 2](!roll20AM --audio,play,nomenu|GUN-Pistolet1) | [Gun 3](!roll20AM --audio,play,nomenu|GUN-Pistolet2) | 
+[Gun 4](!roll20AM --audio,play,nomenu|GUN-Pistolet3) | [Boom](!roll20AM --audio,play,nomenu|FX - Explosion) | [Pompe](!roll20AM --audio,play,nomenu|GUN-Pompe) | 
+[Fusil](!roll20AM --audio,play,nomenu|GUN - Sniper) | [Poing 1](!roll20AM --audio,play,nomenu|FX - Coup de poing) | [Poing 2](!roll20AM --audio,play,nomenu|FX - Coup de poing1) | 
+**HP aura**
+[Menu](!aura) | [ON/OFF](!aura npc)
+**TurnMarker**
+[Menu](!tm)
+**Token Mod**
+[NOM ON](!token-mod --on showname --set defaulttoken --report gm|'Nom affiché') | [NOM OFF](!token-mod --off showname --set defaulttoken --report gm|'Nom caché')
+[TOKEN DEFAUT](!token-mod --set defaulttoken --report gm|'Token par défaut mis à jour')
+[TOKEN PREPA PNJ](! #*GM_Prepa_pnj_maccro)
 }}
