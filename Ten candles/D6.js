e7d3451157b6692532ad17442917923c9ddfa6d6
ctotone <ctotone@gmail.com>
2023-11-06 22:21:16 +0100

    final

--- Ten candles/D6.js
+++ Ten candles/D6.js
@@ -1,25 +1,2 @@
-&{template:quest&#125; {{title=Lancé des D6&#125;&#125; {{subtitle=&#125;&#125;{{Nombre de succés=[[1d6=6]]&#125;&#125;
-|
-2, &{template:quest&#125; {{title=Lancé des D6&#125;&#125; {{subtitle=&#125;&#125;{{Nombre de succés=[[2d6=6]]&#125;&#125;
-|
-3, &{template:quest&#125; {{title=Lancé des D6&#125;&#125; {{subtitle=&#125;&#125;{{Nombre de succés=[[3d6=6]]&#125;&#125;
-|
-4, &{template:quest&#125; {{title=Lancé des D6&#125;&#125; {{subtitle=&#125;&#125;{{Nombre de succés=[[4d6=6]]&#125;&#125;
-|
-5, &{template:quest&#125; {{title=Lancé des D6&#125;&#125; {{subtitle=&#125;&#125;{{Nombre de succés=[[5d6=6]]&#125;&#125;
-|
-6, &{template:quest&#125; {{title=Lancé des D6&#125;&#125; {{subtitle=&#125;&#125;{{Nombre de succés=[[6d6=6]]&#125;&#125;
-|
-7, &{template:quest&#125; {{title=Lancé des D6&#125;&#125; {{subtitle=&#125;&#125;{{Nombre de succés=[[7d6=6]]&#125;&#125;
-|
-8, &{template:quest&#125; {{title=Lancé des D6&#125;&#125; {{subtitle=&#125;&#125;{{Nombre de succés=[[8d6=6]]&#125;&#125;
-|
-9, &{template:quest&#125; {{title=Lancé des D6&#125;&#125; {{subtitle=&#125;&#125;{{Nombre de succés=[[9d6=6]]&#125;&#125;
-|
-10, &{template:quest&#125; {{title=Lancé des D6&#125;&#125; {{subtitle=&#125;&#125;{{Nombre de succés=[[10d6=6]]&#125;&#125;
-}
-
-
-
 &{template:quest} {{title=Lancé des D6}} {{subtitle=}}{{Succés ?=[[1d12>9]]}}
 &{template:quest} {{title=Lancé des D6}} {{subtitle=}}{{Nombre de succés=[[?{Nombre de dés}d6=6]]}}
