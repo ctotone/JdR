9029665b0bff881425f392e0af1b5ac809b804f8
ctotone <ctotone@gmail.com>
2023-12-07 16:00:34 +0100

    Update Aide.js

--- Dirty/macro/Aide.js
+++ Dirty/macro/Aide.js
@@ -1,4 +1,4 @@
-?{Choix de la compétence|
+?{Type d'aide|
 ,|
 Modificateur de défi,
 &{template:classic&#125; {{title=Modificateur défi&#125;&#125;{{info=Tension et Modificateur difficulté 
@@ -52,6 +52,76 @@ Le résultat d'un dé peut être réduit à 0.&#125;&#125;
 |
 Effroi,
 &{template:classic&#125; {{title=Effroi&#125;&#125;{{info=Lancer autant de dés que le MJ l'indique.
+Seul le dé présentant le résultat le plus élevé est retenu.
+
+On coche la case correspondante de la piste de d'équilibre.&#125;&#125;
+&{template:classic&#125; {{title=Résultat du dé dé&#125;&#125;{{info=1-3 Troublé
+4-5 Ébranlé
+6 Dévasté&#125;&#125;
+&{template:classic&#125; {{title=Crise&#125;&#125;{{info=Un double sur les dés déclenche une Crise.&#125;&#125;
+&{template:classic&#125; {{title=Encaisser les dommages&#125;&#125;{{info=Un joueur peut dépenser 1 point de Réserve RAISONNER (non cumulatif) afin de diminuer un dé de d'Éffroi de 1.
+Cela lui permettant d'éviter une Crise ou d'amoindrir la gravité des aggressions mentales que son personnage subit.
+Le résultat d'un dé peut être réduit à 0.&#125;&#125;
+}
+
+
+
+
+
+
+
+//
+// GM Macro
+//
+                                                                    
+?{Type d'aide|
+,|
+Modificateur de défi,/w gm &{template:classic&#125; {{title=Modificateur défi&#125;&#125;{{info=Tension et Modificateur difficulté 
+Reserve de capacité&#44; Lien et Profil
+Etats
+Equipement &#125;&#125;
+|
+Tour de combat,/w gm &{template:classic&#125; {{title=Tour de combat&#125;&#125;{{info=Chaque personnage joue selon son ordre d'Initiative.
+Chaque personnage pourra faire une action de deplacement (10m max.) et une action au choix dans la liste suivante :
+
+Action libre
+Combat corps à corps
+Combat à distance
+Défense
+Un second deplacement de 10m.&#125;&#125;
+&{template:classic&#125; {{title=Action multiple&#125;&#125;{{info=Si un personnage veut faire plus d'une action durant le tour.
+Chaque action en plus de la première subit un malus cumulatif de -20.
+
+Le fait de se battre au corps à corps&#44; même si c'est pour se défendre avant même son rang d'Initiative&#44; compte dans le décompte des actions.&#125;&#125;
+|
+Corps à corps,/w gm &{template:classic&#125; {{title=Combat corps à corps&#125;&#125;{{info=les deux compétences AFFRONTER s'opposent.
+Le joueur lance toujours le dé de Tension et se l'applique qu'il attaque ou se défende.
+Le PNJ ne lance pas le dés de Tension.
+Celui qui l'emporte applique les dégâts&#44; qu'il soit attaquant ou défenseur.&#125;&#125;
+|
+Combat à distance,/w gm &{template:classic&#125; {{title=Combat à distance&#125;&#125;{{info=La compétence AFFRONTER est utilisée.
+L'attaquant (Joueur ou PNJ) lance un dé de Tension et se l'applique.
+En cas de Réussite remarquable&#44; on ajoute un Dés de dommages.
+La difficulté est en fonction de la distance.&#125;&#125;
+&{template:classic&#125; {{title=Distance et Difficulté&#125;&#125;{{info=Bout portant / 30
+Proche / 50
+Moyenne / 70
+Lointaine / 90&#125;&#125;
+|
+Dommages,/w gm &{template:classic&#125; {{title=Dommages&#125;&#125;{{info=Lancer le nombre de dés équivalent au Dommages de l'arme.
+Seul le dé présentant le résultat le plus élevé est retenu.
+Le score relevé détermine le type de Blessure subie.
+
+On coche la case correspondante de la piste de blessure.&#125;&#125;
+&{template:classic&#125; {{title=Résultat du dé dé&#125;&#125;{{info=1-3 Blessure légère
+4-5 Blessure sérieuse
+6 Blessure grave&#125;&#125;
+&{template:classic&#125; {{title=Effet critique&#125;&#125;{{info=Un double sur les dés déclenche l'Effet critique de la source de dommage.&#125;&#125;
+&{template:classic&#125; {{title=Encaisser les dommages&#125;&#125;{{info=Un joueur peut dépenser 1 point de Réserve ENDURER (non cumulatif) afin de diminuer un dé de Dommage de 1.
+Cela lui permettant d'éviter un Effet critique ou d'amoindrir la gravité des blessures que son personnage subit.
+Le résultat d'un dé peut être réduit à 0.&#125;&#125;
+|
+Effroi,/w gm &{template:classic&#125; {{title=Effroi&#125;&#125;{{info=Lancer autant de dés que le MJ l'indique.
 Seul le dé présentant le résultat le plus élevé est retenu.
 
 On coche la case correspondante de la piste de d'équilibre.&#125;&#125;
