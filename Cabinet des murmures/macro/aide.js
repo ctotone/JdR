1b53dadd4554f4bcb6cd9885c83f6ff8384f340c
Evilbram <ctotone@gmail.com>
2024-02-08 14:38:16 +0100

    Create aide.js

---
+++ Cabinet des murmures/macro/aide.js
@@ -0,0 +1,40 @@
+?{Choix de l'aide|
+,|
+Périsprit,
+&{template:cabinet-pouvoir&#125;{{title=Périsprit&#125;&#125;{{=Le périsprit permet d'effectuer les actions suivantes :&#125;&#125;  {{&nbsp;=Lancers de dés&#44; +1 à votre action pour chaque périsprit dépensé&#125;&#125; {{&nbsp;&nbsp;=Utilisation de vos pouvoirs&#125;&#125;{{&nbsp;&nbsp;&nbsp;=Soigner le corps&#44; 1 cercle de santé soigné pour chaque périsprit dépensé&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;=Faire parler le corps avec votre propre voix pour 1 point de périsprit&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=Faire apparaitre pour la durée d'une scène (et tant que vous êtes le comédien) un de vos objets dans le monde matériel vous coute 1 point de périsprit.&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=Dans votre jardin secret&#44; dépensez 1 point pour obtenir un de vos souvenirs.&#125;&#125;
+,|
+Embéllie,
+&{template:cabinet-pouvoir&#125;{{title=Embéllie&#125;&#125;{{=Lors d'un jet d'embellie&#44; on lance le nombre de dés correspondant à la qualité utilisée. 
+
+On comptabilise les 6 et les 1 qui s'annulent mutuellement.
+
+Si il reste au moins un 6&#44; alors on obtient une embéllie. 
+Si il ne reste qu'un 1&#44; il s'agit d'un désastre.&#125;&#125;
+,|
+Difficulté,
+&{template:cabinet-pouvoir&#125;{{title=Difficulté&#125;&#125;{{Aisée=3&#125;&#125;{{Moyenne=6&#125;&#125;{{Ardue=10&#125;&#125;{{Extrême=15&#125;&#125;{{Impossible=21&#125;&#125;
+,|
+Transformer un échec,
+&{template:cabinet-pouvoir&#125;{{title=Transformer un échec&#125;&#125;{{=Au prix d'un point de défaut attaché à la qualité ayant servi à faire l'action&#44; vous pouvez transformer un échec ou un désastre en une réussite&#125;&#125;{{&nbsp;=Si la valeur du défaut devient égal ou supérieur à la qualité associée&#44; la case de l'arbre de vie est bloquée par l'Esprit&#125;&#125;{{&nbsp;&nbsp;=Si la valeur d'un défaut atteint 5&#44; vous obtenez une corruption.&#125;&#125;
+}
+
+
+
+GM
+
+
+?{Choix de l'aide|
+,|
+Périsprit,/w gm &{template:cabinet-pouvoir&#125;{{title=Périsprit&#125;&#125;{{=Le périsprit permet d'effectuer les actions suivantes :&#125;&#125;  {{&nbsp;=Lancers de dés&#44; +1 à votre action pour chaque périsprit dépensé&#125;&#125; {{&nbsp;&nbsp;=Utilisation de vos pouvoirs&#125;&#125;{{&nbsp;&nbsp;&nbsp;=Soigner le corps&#44; 1 cercle de santé soigné pour chaque périsprit dépensé&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;=Faire parler le corps avec votre propre voix pour 1 point de périsprit&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=Faire apparaitre pour la durée d'une scène (et tant que vous êtes le comédien) un de vos objets dans le monde matériel vous coute 1 point de périsprit.&#125;&#125;{{&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=Dans votre jardin secret&#44; dépensez 1 point pour obtenir un de vos souvenirs.&#125;&#125;
+,|
+Embéllie,/w gm &{template:cabinet-pouvoir&#125;{{title=Embéllie&#125;&#125;{{=Lors d'un jet d'embellie&#44; on lance le nombre de dés correspondant à la qualité utilisée. 
+
+On comptabilise les 6 et les 1 qui s'annulent mutuellement.
+
+Si il reste au moins un 6&#44; alors on obtient une embéllie. 
+Si il ne reste qu'un 1&#44; il s'agit d'un désastre.&#125;&#125;
+,|
+Difficulté,/w gm &{template:cabinet-pouvoir&#125;{{title=Difficulté&#125;&#125;{{Aisée=3&#125;&#125;{{Moyenne=6&#125;&#125;{{Ardue=10&#125;&#125;{{Extrême=15&#125;&#125;{{Impossible=21&#125;&#125;
+,|
+Transformer un échec,/w gm &{template:cabinet-pouvoir&#125;{{title=Transformer un échec&#125;&#125;{{=Au prix d'un point de défaut attaché à la qualité ayant servi à faire l'action&#44; vous pouvez transformer un échec ou un désastre en une réussite&#125;&#125;{{&nbsp;=Si la valeur du défaut devient égal ou supérieur à la qualité associée&#44; la case de l'arbre de vie est bloquée par l'Esprit&#125;&#125;{{&nbsp;&nbsp;=Si la valeur d'un défaut atteint 5&#44; vous obtenez une corruption.&#125;&#125;
+}
