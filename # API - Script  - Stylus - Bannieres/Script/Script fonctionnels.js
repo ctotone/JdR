6b4878212c6ded3aa45a0240af8b692fda55b4bc
Evilbram <ctotone@gmail.com>
2024-03-24 11:28:25 +0100

    Final

--- # API - Script  - Stylus - Bannieres/Script/Script fonctionnels.js
+++ # API - Script  - Stylus - Bannieres/Script/Script fonctionnels.js
@@ -1,6 +1,5 @@
 // Script de calcul malus total
 
-<script type="text/worker">
 // On defini quand le sscript s'execute
 on("sheet:opened change:tete_malus change:bras_droit_malus change:bras_gauche_malus change:torse_malus change:jambe_droite_malus change:jambe_gauche_malus", function() {
 	// On récupère les valeur que l'on souhaite
@@ -19,12 +18,11 @@ on("sheet:opened change:tete_malus change:bras_droit_malus change:bras_gauche_ma
         });
     });
 });
-</script>
 
 
 // Script de calcul de valeur en fonction de la valeur d'une autre
 
-<script type="text/worker">
+
 on("sheet:opened change:dot_jambe_gauche", function() {
     getAttrs(["dot_jambe_gauche"], function(values) {
          let dot_jambe_gauche = parseInt(values["dot_jambe_gauche"]) || 0;
@@ -38,11 +36,10 @@ on("sheet:opened change:dot_jambe_gauche", function() {
         });
     });
 });
-</script>
 
 // Script d'attribut en fonction d'une checkbox ou radio
 
-<script type="text/worker">
+
 const dot_jambe_gaucheValues = ["1","2","3","4","5","6","7"];
 	  dot_jambe_gaucheValues.forEach(function(value) {
 		on(`clicked:dot_jambe_gauche_${value}`, function() {
@@ -51,12 +48,10 @@ const dot_jambe_gaucheValues = ["1","2","3","4","5","6","7"];
 		  });
 		});
 	  });
-</script>
 
 // Script d'un boutton mettant à jour un attribut
 // HTML ==> <button type="action" name="act_intro-cabinet">Médium</button>
 
-<script type="text/worker">
    on('clicked:intro-cabinet', function() {
 
   console.log("Activate button clicked");
@@ -67,11 +62,10 @@ const dot_jambe_gaucheValues = ["1","2","3","4","5","6","7"];
 		});
 	});
 });
-</script>
 
 // Script avec integration du fichier de tad
 
-<script type="text/worker">
+
 on("sheet:opened change:data", function() {
     getAttrs(["data"], function(values) {
 			let data = parseInt(values["data"]) || 0;
@@ -84,4 +78,37 @@ on("sheet:opened change:data", function() {
         });
     });
 });
-</script>
+
+
+// Script pour utiliser une value dans un act_
+
+
+const aptitude_Values = ["1","2","3","4","5","6","7","8","9","10","11","12"];
+    aptitude_Values.forEach(function(value) {
+    on(`clicked:aptitude_${value}`, (info) => {
+        startRoll("&{template:relance} {{test=oui}} {{titre=[[0[resultat]]]}} {{d1=[[d6]]}} {{relance=[[0[resultat]]]}}", (results) => {
+             
+            let d1 = parseInt(results.results.d1.dice);
+            let d1bis = parseInt(results.results.d1bis.dice);
+            let computed_d1bis = 0;
+            let computed_relance = 0;
+    
+                if (d1 > 3) reussite_1 = 1, computed_d1 = d1;
+                else reussite_1 = 0, computed_d1 = d1;
+                if (d1 > 5) computed_d1bis = d1bis, computed_relance = 1;
+                else computed_d1bis = d1bis, computed_relance = 0;
+
+
+                finishRoll(
+                    results.rollId,
+                    {
+                        titre: value,
+                        d1: computed_d1,
+                        d1bis: computed_d1bis,
+                        relance: computed_relance
+
+                    }
+             );
+        }); 
+    });
+});
